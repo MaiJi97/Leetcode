9d8375c6fa96d8b15bcef7ebf0a1dd71161714d3
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-05-27 14:00:54 -0400

    Update ReverseLinkedlist1.java

--- java/206_ReverseLinkedlist/ReverseLinkedlist1.java
+++ java/206_ReverseLinkedlist/ReverseLinkedlist1.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public ListNode reverseList1(ListNode head) {
+        if(head == null) {
+            return null;
+        }
+        else if(head.next == null) {
+            return head;
+        }
+        else {
+            ListNode rest = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return rest;
+        }
+    }
+}
