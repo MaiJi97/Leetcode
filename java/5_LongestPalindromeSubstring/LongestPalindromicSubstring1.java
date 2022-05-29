bb239be027c7ab20e9ac97b2fc057582673596ed
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-05-29 11:48:51 -0400

    Update LongestPalindromicSubstring1.java

--- java/5_LongestPalindromeSubstring/LongestPalindromicSubstring1.java
+++ java/5_LongestPalindromeSubstring/LongestPalindromicSubstring1.java
@@ -1,1 +1,26 @@
-
+class Solution {
+    public String longestPalindrome(String s) {
+        int maxlen = 0;
+        String maxstr = "";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                if (isPalindrome(s.substring(i, j+1))) {
+                    if((j + 1 - i) > maxlen){
+                        maxlen = j + 1 - i;
+                        maxstr = s.substring(i, j+1);
+                    }
+                }
+            }
+        }
+        return maxstr;
+    }
+    
+    public boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length()/2; i++){
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
