b2c7ce2f6217771c8d98a4e56285f7af2cbf85ca
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-05-29 11:49:21 -0400

    Update LinkedlistCycle.java

--- java/141_LinkedlistCycle/LinkedlistCycle.java
+++ java/141_LinkedlistCycle/LinkedlistCycle.java
@@ -1,14 +1,14 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null){
+        if (head == null || head.next == null) {
             return false;
         }
         ListNode fast = head;
         ListNode slow = head;
-        while(fast != null && fast.next != null){
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
-            if(fast == slow){
+            if (fast == slow){
                 return true;
             }
         }
