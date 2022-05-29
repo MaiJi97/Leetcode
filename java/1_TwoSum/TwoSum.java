f59b18c435eb3f5ea898ad514fcf482515fb2c33
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-05-28 22:22:24 -0400

    Update TwoSum.java

--- java/1_TwoSum/TwoSum.java
+++ java/1_TwoSum/TwoSum.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int[] ret = new int[2];
+        
+        for(int i=0; i< nums.length; i++) {
+            if(hm.containsKey(target-nums[i])) {
+                ret[0]= hm.get(target-nums[i]);
+                ret[1] = i;
+                break;
+            }              
+            else {
+                hm.put(nums[i], i);
+            }      
+        }
+        return ret;
+    }
+}
