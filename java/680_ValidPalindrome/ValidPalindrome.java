38d82e854d09b0feb446566f3fbf9f2ea7c38f2b
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-06-04 19:07:53 -0400

    Update ValidPalindrome.java

--- java/680_ValidPalindrome/ValidPalindrome.java
+++ java/680_ValidPalindrome/ValidPalindrome.java
@@ -1,1 +1,33 @@
-
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                if (isPalindrome(s, left+1, right)) {
+                    return true;
+                }
+                else if (isPalindrome(s, left, right-1)) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    public boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
