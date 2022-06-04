f17237f14ff8093deb061960350edab42c664430
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-06-04 17:35:26 -0400

    Update ValidPalindrome.java

--- java/125_ValidPalindrome/ValidPalindrome.java
+++ java/125_ValidPalindrome/ValidPalindrome.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public boolean isPalindrome(String s) {
+        String cleanString = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetterOrDigit(s.charAt(i))) {
+                cleanString += s.charAt(i);
+            }
+        }
+        cleanString = cleanString.toLowerCase();
+        int left = 0;
+        int right = cleanString.length() - 1;
+        while (left < right) {
+            if (cleanString.charAt(left) != cleanString.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
