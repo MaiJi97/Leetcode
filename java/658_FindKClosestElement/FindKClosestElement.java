31cc9d80bc71766737620739049dbed438d67720
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-06-08 23:51:23 -0400

    Update FindKClosestElement.java

--- java/658_FindKClosestElement/FindKClosestElement.java
+++ java/658_FindKClosestElement/FindKClosestElement.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         List<Integer> ret = new ArrayList<>();;
-        int left = 0;
-        int right = arr.length - k;
+        int low = 0;
+        int high = arr.length - k;
         
         // ---|--------------x-----|--- 1st case
         // ---|-----x--------------|--- 2nd case
         
-        while (left < right) {
-            int mid = left + (right - left) / 2; // mid represents the left boundary of the k numbers
+        while (low < high) {
+            int mid = low + (high - low) / 2; // mid represents the left boundary of the k numbers
             if (x - arr[mid] > arr[mid+k] - x) { // 1st case
-                left = mid + 1;
+                low = mid + 1;
             }
             else {
-                right = mid;
+                high = mid;
             }
         }
         
         for (int i = 0; i < k; i++) {
-            ret.add(arr[left + i]);
+            ret.add(arr[low + i]);
         }
         return ret;
     }
