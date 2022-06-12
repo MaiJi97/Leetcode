0d371199e0d66bb8386799e4e503bcceb3e8de66
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-06-12 17:20:11 -0400

    Update FlattenBinaryTreeToLinkedList.java

--- java/114_FlattenBinaryTreeToLinkedList/FlattenBinaryTreeToLinkedList.java
+++ java/114_FlattenBinaryTreeToLinkedList/FlattenBinaryTreeToLinkedList.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            return;
+        }
+        
+        if (root.left != null) {
+            flatten(root.left);
+            TreeNode temp = root.right; // store the initial right subtree
+            root.right = root.left; // insert the flattened left subtree
+            root.left = null; // set the left subtree to null
+            TreeNode curr = root;
+            while (curr.right != null) {
+                curr = curr.right;
+            }
+            curr.right = temp;        
+        }
+        flatten(root.right);    
+    }
+}
