6b43034097d15e4f0aede97e383c8d877e23a912
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-06-11 23:27:40 -0400

    Update BinaryTreeLevelOrderTraversal.java

--- java/102_BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.java
+++ java/102_BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.java
@@ -1,1 +1,28 @@
-
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        
+        if (root == null) {
+            return ret;
+        }
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        
+        while (!q.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = q.poll();
+                level.add(temp.val);
+                if (temp.left != null) {
+                    q.offer(temp.left);
+                }
+                if (temp.right != null) {
+                    q.offer(temp.right);
+                }
+            }
+            ret.add(level);
+        }
+        return ret;
+    }
+}
