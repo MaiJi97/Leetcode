7acfa68ebab22c9e4cdbaee5b98a24eda582a076
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-06-12 12:06:50 -0400

    Update BinaryTreePaths.java

--- java/257_BinaryTreePaths/BinaryTreePaths.java
+++ java/257_BinaryTreePaths/BinaryTreePaths.java
@@ -1,1 +1,26 @@
-
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ret = new ArrayList<>();
+        pathHelper(ret, "", root);
+        return ret;
+    }
+    
+    public void pathHelper(List<String> ret, String path, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        
+        path += root.val;
+        
+        if (root.left == null && root.right == null) {
+            ret.add(path);
+            return;
+        }
+        if (root.left != null) {
+            pathHelper(ret, path + "->", root.left);
+        }
+        if (root.right != null) {
+            pathHelper(ret, path + "->", root.right);
+        }
+    }
+}
