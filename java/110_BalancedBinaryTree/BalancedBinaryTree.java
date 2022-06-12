9e848d0bb65171a1e403d0c48080c8da14000d5f
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-06-12 13:00:28 -0400

    Update BalancedBinaryTree.java

--- java/110_BalancedBinaryTree/BalancedBinaryTree.java
+++ java/110_BalancedBinaryTree/BalancedBinaryTree.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        else {
+            return (Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1) && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+    
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    }
+}
