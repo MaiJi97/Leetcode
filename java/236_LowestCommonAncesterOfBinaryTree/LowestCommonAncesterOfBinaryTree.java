a974c75f8d43eb89680090ce6ce376eae883503d
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-06-12 15:14:19 -0400

    Update LowestCommonAncesterOfBinaryTree.java

--- java/236_LowestCommonAncesterOfBinaryTree/LowestCommonAncesterOfBinaryTree.java
+++ java/236_LowestCommonAncesterOfBinaryTree/LowestCommonAncesterOfBinaryTree.java
@@ -1,1 +1,27 @@
-
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) { // if root is p or q
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if (left != null && right != null) { // if p and q are in two different subtrees
+            return root;
+        }
+        
+        if (left != null) { // if left subtree has a LCA
+            return left;
+        }
+        
+        
+        if (right != null) { // if right subtree has a LCA
+            return right;
+        }
+        
+        return null;
+    }
+}
