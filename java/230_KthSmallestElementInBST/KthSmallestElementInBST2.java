a853c92d19776dd59081d5071b27e693949a19a5
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-06-16 22:29:01 -0400

    Update KthSmallestElementInBST2.java

--- java/230_KthSmallestElementInBST/KthSmallestElementInBST2.java
+++ java/230_KthSmallestElementInBST/KthSmallestElementInBST2.java
@@ -1,17 +1,18 @@
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        Stack<TreeNode>() stack = new Stack<>();
-        while (true) {
-            while (root != null) {
-                stack.push(root.val);
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || root != null) {
+            while(root != null) {
+                stack.push(root);
                 root = root.left;
             }
             root = stack.pop();
             k--;
-            if (k == 0) {
+            if(k == 0) {
                 return root.val;
             }
             root = root.right;
         }
+        return 0;
     }
 }
