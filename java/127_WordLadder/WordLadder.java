a882c9d29c7a83cdb1e3d098668c6b9d49bdd3ab
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-06-22 23:55:18 -0400

    Update WordLadder.java

--- java/127_WordLadder/WordLadder.java
+++ java/127_WordLadder/WordLadder.java
@@ -1,1 +1,33 @@
-
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String> set = new HashSet<String>(wordList);
+        Queue<String> queue = new ArrayDeque<>();
+        HashSet<String> visited = new HashSet<>();
+        visited.add(beginWord);
+        queue.offer(beginWord);
+        
+        int length = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String word = queue.poll();
+                if (word.equals(endWord)) {
+                    return length;
+                }
+                for (int j = 0; j < word.length(); j++) {
+                    for (char k = 'a'; k <= 'z'; k++) {
+                        char[] newWordArr = word.toCharArray();
+                        newWordArr[j] = (char)k;
+                        String newWord = new String(newWordArr);
+                        if (set.contains(newWord) && !visited.contains(newWord)) {
+                            visited.add(newWord);
+                            queue.add(newWord);
+                        }
+                    }
+                }
+            }
+            length++;
+        }
+        return 0;
+    }
+}
