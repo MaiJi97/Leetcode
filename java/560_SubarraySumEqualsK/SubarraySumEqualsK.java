703b5efc7cbd3899fd35c46dce5cc1147ce43eec
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-07-24 12:58:09 -0400

    Create SubarraySumEqualsK.java

---
+++ java/560_SubarraySumEqualsK/SubarraySumEqualsK.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, 1);
+        int totalPrefixSum = 0;
+        int ret = 0;
+        for (int i = 0; i < nums.length; i++) {
+            totalPrefixSum += nums[i];
+            int prevPrefixSum = totalPrefixSum - k;
+            if (hm.containsKey(prevPrefixSum)) {
+                ret += hm.get(prevPrefixSum);
+            }
+            hm.put(totalPrefixSum, hm.getOrDefault(totalPrefixSum, 0) + 1);
+        }
+        return ret;
+    }
+}
