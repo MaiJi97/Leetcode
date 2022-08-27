61779061ea1a77a745d8eb3fe86042c5761b2d9e
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-08-27 09:52:44 -0500

    Update LongestPalindromicSubsequence.java

--- java/516_LongestPalindromeSubsequence/LongestPalindromicSubsequence.java
+++ java/516_LongestPalindromeSubsequence/LongestPalindromicSubsequence.java
@@ -4,22 +4,22 @@ class Solution {
             return s.length();
         }
         int n = s.length();
-        int[][] OPT = new int[n][n];
+        int[][] dp = new int[n][n];
         for (int i = n - 2; i >= 0; i--) {
             for (int j = i; j < n; j++) {
                 if (s.charAt(i) == s.charAt(j)) {
                     if (j - i <= 2) {
-                        OPT[i][j] = j - i + 1;
+                        dp[i][j] = j - i + 1;
                     }
                     else {
-                        OPT[i][j] = OPT[i + 1][j - 1] + 2;
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
                     }
                 }
                 else {
-                    OPT[i][j] = Math.max(OPT[i+1][j], OPT[i][j-1]);
+                    OPT[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
                 }
             }
         }
-        return OPT[0][n-1];
+        return dp[0][n-1];
     }
 }
