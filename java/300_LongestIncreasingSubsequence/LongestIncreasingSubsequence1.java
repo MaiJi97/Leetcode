cb2d320fc510089b78ec7123bf25f70938234294
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-08-27 09:52:06 -0500

    Update LongestIncreasingSubsequence1.java

--- java/300_LongestIncreasingSubsequence/LongestIncreasingSubsequence1.java
+++ java/300_LongestIncreasingSubsequence/LongestIncreasingSubsequence1.java
@@ -1,17 +1,17 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] DP = new int[nums.length];
-        DP[0] = 1;
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
         int ret = 1;
         for (int i = 1; i < nums.length; i++) {
             int max = 0;
             for (int j = 0; j < i; j++) {
                 if (nums[j] < nums[i]) {
-                    max = Math.max(max, DP[j]);
+                    max = Math.max(max, dp[j]);
                 }
             } 
-            DP[i] = max + 1;
-            ret = Math.max(ret, DP[i]);
+            dp[i] = max + 1;
+            ret = Math.max(ret, dp[i]);
         }
         return ret;
     }
