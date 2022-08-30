64e57c7544b1a430b7354d001b9e3e3ab3d3cbb0
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-08-29 20:09:24 -0500

    Update MaximumLengthOfRepeatedSubarray.java

--- java/718_MaximumLengthOfRepeatedSubarray/MaximumLengthOfRepeatedSubarray.java
+++ java/718_MaximumLengthOfRepeatedSubarray/MaximumLengthOfRepeatedSubarray.java
@@ -12,7 +12,7 @@ class Solution {
         }
         for (int i = 1; i < l1 + 1; i++) {
             for (int j = 1; j < l2 + 1; j++) {
-                if (nums1[i-1] == nums2[j-1]) {
+                if (nums1[i - 1] == nums2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                     ret = Math.max(ret, dp[i][j]);
                 }
