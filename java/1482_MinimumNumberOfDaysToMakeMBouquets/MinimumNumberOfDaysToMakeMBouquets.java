10f9b8542264281ca99049adeb92b0b6f720d5ef
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-10-26 16:24:48 -0500

    Create MinimumNumberOfDaysToMakeMBouquets.java

---
+++ java/1482_MinimumNumberOfDaysToMakeMBouquets/MinimumNumberOfDaysToMakeMBouquets.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        if (bloomDay.length < m * k) return -1;
+        int left = Integer.MAX_VALUE; // min value in bloomDay
+        int right = Integer.MIN_VALUE; // max value in bloomDay
+        for (int i = 0; i < bloomDay.length; i++) {
+            if (bloomDay[i] > right) {
+                right = bloomDay[i];
+            }
+            if (bloomDay[i] < left) {
+                left = bloomDay[i];
+            }
+        }
+        
+        while (left < right) { // pick m subarrays of length k, max of each subarray is minimized
+            int mid = left + (right - left) / 2;
+            if (helper(bloomDay, mid, k) < m) { // return number of subarray whose max value is mid (number of bouquets can be made on a given day)
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return left; 
+    }
+    
+    public int helper(int[] bloomDay, int mid, int k) {
+        int count = 0;
+        int flowers = 0;
+        for (int i = 0; i < bloomDay.length; i++) {
+            if (bloomDay[i] <= mid) {
+                flowers++;
+                if (flowers == k) {
+                    count++;
+                    flowers = 0;
+                }
+            }
+            else {
+                flowers = 0;
+            }
+        }
+        return count;
+    }
+}
