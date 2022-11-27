76d4d51be3f15740cf8c08e758e840011c45558d
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-11-26 23:58:38 -0600

    Create MergeIntervals.java

---
+++ java/56_MergeIntervals/MergeIntervals.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            return a[0] - b[0];
+        });
+        List<int[]> ret = new ArrayList<>();
+        int[] newInterval = intervals[0];
+        ret.add(newInterval);
+        for (int i = 0; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+            if (interval[0] <= newInterval[1]) {
+                newInterval[1] = Math.max(interval[1], newInterval[1]);
+            }
+            else {
+                ret.add(interval);
+                newInterval = interval;
+            }
+        }
+        return ret.toArray(new int[ret.size()][]);    
+    }
+}
