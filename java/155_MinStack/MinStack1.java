1f81de63c3bd26d99fc21760eb96bd96677c5b14
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-12-21 11:32:25 -0600

    Create MinStack1.java

---
+++ java/155_MinStack/MinStack1.java
@@ -0,0 +1,48 @@
+class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        this.stack = new Stack<>();
+        this.minStack = new Stack<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if (stack.isEmpty()) {
+            return;
+        }
+        if (minStack.peek().equals(stack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        if (stack.isEmpty()) {
+            return -1;
+        }
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
