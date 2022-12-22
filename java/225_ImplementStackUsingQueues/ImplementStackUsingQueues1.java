49c7b02facb568ef11d5e43794984926f5d2dcff
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-12-21 20:35:15 -0600

    Create ImplementStackUsingQueues1.java

---
+++ java/225_ImplementStackUsingQueues/ImplementStackUsingQueues1.java
@@ -0,0 +1,41 @@
+class MyStack {
+
+    Queue<Integer> q1 = new ArrayDeque<>();
+    Queue<Integer> q2 = new ArrayDeque<>();
+
+    public MyStack() {
+        
+    }
+    
+    public void push(int x) {
+        q2.offer(x);
+        while (!q1.isEmpty()) {
+            q2.offer(q1.poll());
+        }
+        while (!q2.isEmpty()) {
+            q1.offer(q2.poll());
+        }
+    }
+    
+    public int pop() {
+        
+        return q1.poll();
+    }
+    
+    public int top() {
+        return q1.peek();
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
