39da50b13192bf899bd808cedd948285a87146fd
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2022-12-25 15:44:37 -0600

    Create AddTwoNumbers1.java

---
+++ java/445_AddTwoNumbers/AddTwoNumbers1.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+        int carry = 0;
+        ListNode head = null;
+        while (l1 != null || l2 != null) {
+            int l1Val = l1 == null ? 0 : l1.val;
+            int l2Val = l2 == null ? 0 : l2.val;
+            int num = l1Val + l2Val + carry;
+            ListNode node = new ListNode(num % 10); 
+            node.next = head; // append the new node to the front
+            head = node;
+            carry = num / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry != 0) {
+            ListNode node = new ListNode(carry); 
+            node.next = head;
+            head = node;
+        }
+        return head;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
