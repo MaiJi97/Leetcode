dfee110a49e7bf4b2f110cb9aae82cded2d40b5d
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-01-06 17:46:42 -0600

    Create MeetingRooms4.java

---
+++ java/253_MeetingRooms/MeetingRooms4.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int[] starts = new int[intervals.length];
+        int[] ends = new int[intervals.length];
+        for (int i = 0; i < intervals.length; i++) {
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int endIndex = 0;
+        int ret = 0;
+        for (int i = 0; i < intervals.length; i++) {
+            if (starts[i] < ends[endIndex]) {
+                ret++;
+            }
+        }
+        return ret;
+    }
+}
