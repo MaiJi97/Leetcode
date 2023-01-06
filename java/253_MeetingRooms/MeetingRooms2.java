ebdbd1bebf04fe1e1fe065c08f80b276bc9d16ef
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-01-06 17:12:54 -0600

    Update MeetingRooms2.java

--- java/253_MeetingRooms/MeetingRooms2.java
+++ java/253_MeetingRooms/MeetingRooms2.java
@@ -1,14 +1,16 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]); // sort by start time
-        Queue<Integer> q = new PriorityQueue<Integer>((a, b) -> a - b); // sort by end time
-        q.offer(intervals[0][1]);
-        for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] >= q.peek()) { // a new start is later than the earliest end time
-                q.poll();
-            }
-            q.offer(intervals[i][1]);
+        Queue<int[]> q = new PriorityQueue<int[]>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        for (int[] interval : intervals) {
+            q.offer(new int[]{interval[0], 1});
+            q.offer(new int[]{interval[1], -1});
         }
-        return q.size();  
+        int ret = 0;
+        int count = 0;
+        while (!q.isEmpty()) {
+            count += q.poll()[1];
+            ret = Math.max(ret, count);
+        }
+        return ret;
     }
 }
