fa83b21031f7d317cfc48c0f024ee888d0b6a941
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-01-06 16:51:41 -0600

    Update MeetingRooms.java

--- java/252_MeetingRooms/MeetingRooms.java
+++ java/252_MeetingRooms/MeetingRooms.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        int latest = 0;
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         for (int i = 1; i < intervals.length; i++) {
-            latest = Math.max(latest, intervals[i-1][1]);
-            if (intervals[i][0] < latest) {
+            if (intervals[i][0] < intervals[i-1][1]) {
                 return false;
             }
         }
