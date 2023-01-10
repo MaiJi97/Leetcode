042187e3df07ae30004cfa3fdd6c44ae63fa08e5
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-01-10 14:17:49 -0600

    Create MinimumDepthOfBinaryTree2.java

---
+++ java/111_MinimumDepthOfBinaryTree/MinimumDepthOfBinaryTree2.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> q = new ArrayDeque<TreeNode>();
+        q.offer(root);
+        int depth = 1;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = q.poll();
+                if (current.left == null && current.right == null) return depth;
+                if (current.left != null) q.offer(current.left);
+                if (current.right != null) q.offer(current.right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
