a053aa20b40123ae72d40fb6b492c950425495da
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-01-14 21:33:28 -0600

    Create Combinations.java

---
+++ java/77_Combinations/Combinations.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ret = new ArrayList<>();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = i + 1;
+        }
+        dfs(nums, k, ret, new ArrayList<>(), 0);
+        return ret;
+    }
+
+    public void dfs(int[] nums, int k, List<List<Integer>> ret, List<Integer> current, int index) {
+        if (current.size() == k) {
+            ret.add(new ArrayList<>(current));
+        }
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            dfs(nums, k, ret, current, i+1);
+            current.remove(current.size()-1);
+        }
+    }
+}
