391464ed71c4f975ae072076ba36f6b049ff359a
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-05-22 11:31:36 +0800

    Create ImplementTrie.java

---
+++ java/208_ImplementTrie/ImplementTrie.java
@@ -0,0 +1,53 @@
+class Trie {
+
+    TrieNode root;
+
+    public Trie() {
+        this.root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isWord = true;
+    }
+    
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) return false;
+            else node = node.children[c - 'a'];
+        }
+        return node.isWord;
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (node.children[c - 'a'] == null) return false;
+            else node = node.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+
+ class TrieNode {
+     boolean isWord;
+     TrieNode[] children;
+     public TrieNode() {
+         this.children = new TrieNode[26];
+     }
+ }
