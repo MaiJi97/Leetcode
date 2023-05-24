c9f052625cad723b093ef2a6b0df2327224c4d47
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-05-24 15:17:46 +0800

    Create WordSearch.java

---
+++ java/79_WordSearch/WordSearch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (helper(board, word, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean helper(char[][] board, String word, int i, int j, int current) {
+        if (current == word.length()) return true;
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+            return false;
+        }
+
+        else if (word.charAt(current) == board[i][j]) {
+            char temp = board[i][j];
+            board[i][j] = '#';
+            boolean ret = helper(board, word, i+1, j, current + 1) || helper(board, word, i-1, j, current + 1) || helper(board, word, i, j+1, current + 1) || helper(board, word, i, j-1, current + 1);
+            board[i][j] = temp;
+            return ret;
+        }
+        return false;
+    }
+}
