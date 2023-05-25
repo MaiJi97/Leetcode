c09eac81d4c7d6da55f3611ca9de8eac10687b22
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-05-25 15:14:23 +0800

    Update WordSearch.java

--- java/79_WordSearch/WordSearch.java
+++ java/79_WordSearch/WordSearch.java
@@ -16,19 +16,15 @@ class Solution {
     public boolean dfs(char[][] board, boolean[][] visited, String word, int i, int j, int current) {
         if (current == word.length()) return true;
 
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j]) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || word.charAt(current) != board[i][j]) {
             return false;
         }
 
-        else if (word.charAt(current) == board[i][j]) {
-            // char temp = board[i][j];
-            // board[i][j] = '#';
-            visited[i][j] = true;
-            boolean ret = dfs(board, visited, word, i+1, j, current + 1) || dfs(board, visited, word, i-1, j, current + 1) || dfs(board, visited, word, i, j+1, current + 1) || dfs(board, visited, word, i, j-1, current + 1);
-            // board[i][j] = temp;
-            visited[i][j] = false;
-            return ret;
-        }
-        return false;
+        visited[i][j] = true;
+
+        boolean ret = dfs(board, visited, word, i+1, j, current + 1) || dfs(board, visited, word, i-1, j, current + 1) || dfs(board, visited, word, i, j+1, current + 1) || dfs(board, visited, word, i, j-1, current + 1);
+        
+        visited[i][j] = false;
+        return ret;
     }
 }
