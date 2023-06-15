634fbfb19d3f076ae2fb9284834d57c3ca583926
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-06-15 14:00:32 +0800

    Create TrappingRainWater.java

---
+++ java/42_TrappingRainWater/TrappingRainWater.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int trap(int[] height) {
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+        int ret = 0; 
+
+        leftMax[0] = height[0];
+        rightMax[height.length - 1] = height[height.length - 1];
+        for (int i = 1 ; i < height.length; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+        for (int i = height.length-2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+        for (int i = 0; i < height.length; i++) {
+            ret += (Math.min(leftMax[i], rightMax[i]) - height[i]);
+        }
+        return ret;
+    }
+}
