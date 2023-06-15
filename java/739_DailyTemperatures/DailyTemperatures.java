ca88367fa5905c8cb415a6ce71902288dc601bd1
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-06-15 14:54:28 +0800

    Update DailyTemperatures.java

--- java/739_DailyTemperatures/DailyTemperatures.java
+++ java/739_DailyTemperatures/DailyTemperatures.java
@@ -3,7 +3,7 @@ class Solution {
         Stack<int[]> stack = new Stack<>();
         int[] ret = new int[temperatures.length];
         for (int i = temperatures.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && temperatures[i] >= stack.peek()[0]) stack.pop();
+            while (!stack.isEmpty() && stack.peek()[0] <= temperatures[i]) stack.pop();
             ret[i] = stack.isEmpty() ? 0 : stack.peek()[1] - i;
             stack.push(new int[] {temperatures[i], i});
         }
