9d9e869c92a0a0d66d32ccd837566729c78cb382
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-06-20 17:30:06 +0800

    Create LargestRectangleInHistogram1.java

---
+++ java/84_LargestRectangleInHistogram/LargestRectangleInHistogram1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack();
+        int[] leftFirstSmaller = new int[heights.length];
+        int[] rightFirstSmaller = new int[heights.length];
+
+        for (int i = 0; i < heights.length; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) stack.pop();
+            if (stack.isEmpty()) leftFirstSmaller[i] = -1;
+            else leftFirstSmaller[i] = stack.peek(); // 记录左边的next smaller element index
+            stack.push(i);
+        }
+
+        stack = new Stack(); 
+        for (int i = heights.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) stack.pop();
+            if (stack.isEmpty()) rightFirstSmaller[i] = heights.length;
+            else rightFirstSmaller[i] = stack.peek(); // 记录右边的next smaller element index
+            stack.push(i);
+        }
+
+        int ret = 0;
+        for (int i = 0; i < heights.length; i++) {
+            int area = (rightFirstSmaller[i] - leftFirstSmaller[i] - 1) * heights[i];
+            ret = Math.max(ret, area);
+        }
+        return ret;
+    }
+}
