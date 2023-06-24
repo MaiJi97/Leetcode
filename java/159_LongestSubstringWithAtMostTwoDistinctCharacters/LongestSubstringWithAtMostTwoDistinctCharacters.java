09822807bfdf163dafe64700cd491b2cd6e4a694
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-06-24 18:12:45 +0800

    Update LongestSubstringWithAtMostTwoDistinctCharacters.java

--- java/159_LongestSubstringWithAtMostTwoDistinctCharacters/LongestSubstringWithAtMostTwoDistinctCharacters.java
+++ java/159_LongestSubstringWithAtMostTwoDistinctCharacters/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -1,15 +1,16 @@
 class Solution {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
+        int count = 0;
         HashMap<Character, Integer> hm = new HashMap<>();
         int j = 0;
         int ret = 0;
         for (int i = 0; i < s.length(); i++) {
-            while (j < s.length() && hm.size() >= 2 && !hm.containsKey(s.charAt(i))) {
+            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
+            while (j < s.length() && hm.size() > 2) {
                 hm.put(s.charAt(j), hm.get(s.charAt(j)) - 1);
                 if (hm.get(s.charAt(j)) == 0) hm.remove(s.charAt(j));
                 j++;
             }
-            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
             ret = Math.max(ret, i - j + 1);
         }
         return ret;
