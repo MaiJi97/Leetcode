d67af5c62c73c6038abc0110684cff218dd101ed
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-06-24 11:11:13 +0800

    Update SlidingWindowMaximum2.java

--- java/239_SlidingWindowMaximum/SlidingWindowMaximum2.java
+++ java/239_SlidingWindowMaximum/SlidingWindowMaximum2.java
@@ -3,9 +3,9 @@ class Solution {
         Deque<Integer> q = new ArrayDeque<>();
         int[] ret = new int[nums.length - k + 1];
         for (int i = 0; i < nums.length; i++) {
-            int startWindowIndex = i - k + 1; // 每个window的right boundary
-            while (!q.isEmpty() && i - q.peekFirst() >= k) q.pollFirst(); 
-            while (!q.isEmpty() && nums[q.peekLast()] <= nums[i]) q.pollLast();
+            int startWindowIndex = i - k + 1; // i是每个window的right boundary
+            while (!q.isEmpty() && i - q.peekFirst() >= k) q.pollFirst(); // 维持k element window
+            while (!q.isEmpty() && nums[q.peekLast()] <= nums[i]) q.pollLast(); // q中的element按照potentially maximum排序
             q.offerLast(i); 
             if (startWindowIndex >= 0) ret[startWindowIndex] = nums[q.peekFirst()]; // peek first的是当前window最左侧的element，也是最大的element
         }
