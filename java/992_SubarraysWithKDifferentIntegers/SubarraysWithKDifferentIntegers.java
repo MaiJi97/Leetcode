01a4adb1d64bfde87492125b1402cad3b8e5f9ba
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-06-25 18:06:03 +0800

    Update SubarraysWithKDifferentIntegers.java

--- java/992_SubarraysWithKDifferentIntegers/SubarraysWithKDifferentIntegers.java
+++ java/992_SubarraysWithKDifferentIntegers/SubarraysWithKDifferentIntegers.java
@@ -1,22 +1,20 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return countAtMost(nums, k) - countAtMost(nums, k - 1);
+        return atMostK(nums, k) - atMostK(nums, k-1);
     }
-    
-    public int countAtMost(int[] nums, int k) {
-        int ret = 0;
+
+    private int atMostK(int[] nums, int k) {
         HashMap<Integer, Integer> hm = new HashMap<>();
+        int ret = 0;
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
             hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
-            while (hm.size() > k) { // 不能写做size == k,因为会miss掉(1,2,1),(1,2,1,2)的情况
-                hm.put(nums[j], hm.getOrDefault(nums[j], 0) - 1);
-                if (hm.get(nums[j]) == 0) {
-                    hm.remove(nums[j]);
-                }
+            while (j < nums.length && hm.size() > k) {
+                hm.put(nums[j], hm.get(nums[j]) - 1);
+                if (hm.get(nums[j]) == 0) hm.remove(nums[j]);
                 j++;
             }
-            ret += i - j + 1;
+            ret += (i - j + 1);
         }
         return ret;
     }
