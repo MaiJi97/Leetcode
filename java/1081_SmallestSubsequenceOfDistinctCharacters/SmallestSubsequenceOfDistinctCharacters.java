a3e11ef9401a575cea329ac3bd91675ecc596108
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-06-25 09:13:32 +0800

    Create SmallestSubsequenceOfDistinctCharacters.java

---
+++ java/1081_SmallestSubsequenceOfDistinctCharacters/SmallestSubsequenceOfDistinctCharacters.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        int[] lastIndex = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+
+        boolean[] seen = new boolean[26];
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            int current = s.charAt(i) - 'a';
+            if (seen[current]) continue; // if seen continue as we need to pick only one char
+            while (!stack.isEmpty() && current < (stack.peek() - 'a') && lastIndex[stack.peek() - 'a'] > i) seen[stack.pop() - 'a'] = false;
+            stack.push(s.charAt(i));
+            seen[current] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
