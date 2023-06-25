678664bee7f816b7331e5faf859effb17214d41d
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-06-25 18:44:59 +0800

    Create CountNumberOfNiceSubarrays.java

---
+++ java/1248_CountNumberOfNiceSubarrays/CountNumberOfNiceSubarrays.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        return atMostK(nums, k) - atMostK(nums, k-1);
+    }
+
+    private int atMostK( int[] nums, int k) {
+        int ret = 0;
+        int j = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 1) count++;
+            while (j < nums.length && count > k) {
+                if (nums[j] % 2 == 1) count--;
+                j++;
+            }
+            ret += (i - j + 1);
+        }
+        return ret;
+    }
+}
