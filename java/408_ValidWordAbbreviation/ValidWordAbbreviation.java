639b3841d1534cf351e7c06793e2de58abf5c721
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-06-26 14:32:20 +0800

    Update ValidWordAbbreviation.java

--- java/408_ValidWordAbbreviation/ValidWordAbbreviation.java
+++ java/408_ValidWordAbbreviation/ValidWordAbbreviation.java
@@ -7,8 +7,7 @@ class Solution {
                 pointer1++;
                 pointer2++;
             }
-            else if (abbr.charAt(pointer2) < '0' || abbr.charAt(pointer2) > '9') return false; // if the char is not digit
-            else if (abbr.charAt(pointer2) == '0') return false;
+            else if (abbr.charAt(pointer2) <= '0' || abbr.charAt(pointer2) > '9') return false; // if the char is not digit, or is 0
             else {
                 StringBuilder sb = new StringBuilder();
                 while (pointer2 < abbr.length() && Character.isDigit(abbr.charAt(pointer2))) {
