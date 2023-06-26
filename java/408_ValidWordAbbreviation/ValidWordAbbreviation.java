fca643770a39812093ed387fe35ca80debcef76d
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-06-26 12:54:17 +0800

    Create ValidWordAbbreviation.java

---
+++ java/408_ValidWordAbbreviation/ValidWordAbbreviation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int pointer1 = 0;
+        int pointer2 = 0;
+        while (pointer1 < word.length() && pointer2 < abbr.length()) {
+            if (word.charAt(pointer1) == abbr.charAt(pointer2)) {
+                pointer1++;
+                pointer2++;
+            }
+            else if (abbr.charAt(pointer2) < '0' || abbr.charAt(pointer2) > '9') return false; // if the char is not digit
+            else if (abbr.charAt(pointer2) == '0') return false;
+            else {
+                StringBuilder sb = new StringBuilder();
+                while (pointer2 < abbr.length() && Character.isDigit(abbr.charAt(pointer2))) {
+                    sb.append(abbr.charAt(pointer2));
+                    pointer2++;
+                }
+                int length = Integer.parseInt(sb.toString());
+                pointer1 += length;
+            }
+        }
+        return pointer1 == word.length() && pointer2 == abbr.length();
+    }
+}
