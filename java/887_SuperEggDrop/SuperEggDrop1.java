8da79c3feb02d42569513dfcad5a1c68974f832e
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-06-27 18:43:50 +0800

    Create SuperEggDrop1.java

---
+++ java/887_SuperEggDrop/SuperEggDrop1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int superEggDrop(int k, int n) {
+        int[][] memo = new int[k + 1][n + 1];
+        return helper(k, n, memo);
+    }
+
+    private int helper(int k, int n, int[][] memo) {
+        if (n <= 1) return n;
+        if (k <= 1) return n;
+        if (memo[k][n] > 0) return memo[k][n];
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i <= n; i++) {
+            int up = helper(k, n - i, memo);
+            int down = helper(k-1, i - 1, memo);
+            min = Math.min(min, Math.max(up, down) + 1);
+        }
+        memo[k][n] = min;
+        return min;
+    }
+}
