b5dfb3f84972de1b258cab4e839dd0f99f5fabed
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-02 22:12:30 +0800

    Create ContinuousSubarraySum.java

---
+++ java/523_ContinuousSubarraySum/ContinuousSubarraySum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, -1);
+        int totalPrefixSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            totalPrefixSum += nums[i];
+            int remainder = totalPrefixSum % k;
+            if (hm.containsKey(remainder)) {
+                if (i - hm.get(remainder) > 1) return true;
+            }
+            else hm.put(remainder, i);
+        }
+        return false;
+    }
+}
