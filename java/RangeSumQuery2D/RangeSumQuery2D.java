fb7ba2e5513c1318b43eea4f883abfc678054c0b
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-03 18:06:07 +0800

    Create RangeSumQuery2D.java

---
+++ java/RangeSumQuery2D/RangeSumQuery2D.java
@@ -0,0 +1,26 @@
+class NumMatrix {
+
+    int[][] prefixSum;
+
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        prefixSum = new int[m+1][n+1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                prefixSum[i][j] = prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1] + matrix[i-1][j-1];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int ret = prefixSum[row2+1][col2+1] - prefixSum[row2+1][col1] - prefixSum[row1][col2+1] + prefixSum[row1][col1];
+        return ret;
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
