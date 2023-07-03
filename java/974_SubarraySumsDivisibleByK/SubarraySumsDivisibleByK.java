162a61a14e6f5fa5627bed53a527ff065539a560
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-03 14:14:42 +0800

    Update SubarraySumsDivisibleByK.java

--- java/974_SubarraySumsDivisibleByK/SubarraySumsDivisibleByK.java
+++ java/974_SubarraySumsDivisibleByK/SubarraySumsDivisibleByK.java
@@ -1,12 +1,12 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        HashMap<Integer, Integer> hm = new HashMap<>(); // <remainder of prefixSum divided by k, number of occurrence>
         hm.put(0, 1);
         int ret = 0;
-        int totalPrefixSum = 0;
+        int prefixSum = 0;
         for (int i = 0; i < nums.length; i++) {
-            totalPrefixSum += nums[i];
-            int remainder = totalPrefixSum % k;
+            prefixSum += nums[i];
+            int remainder = prefixSum % k;
             if (remainder < 0) {
                 remainder += k; // Because -1 % 5 = -1, but we need the positive mod 4
             }
