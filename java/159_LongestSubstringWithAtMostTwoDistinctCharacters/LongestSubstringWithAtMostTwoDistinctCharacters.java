6a7807257062898987fd20c665e1391350a8eb48
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-04 10:49:12 +0800

    Update LongestSubstringWithAtMostTwoDistinctCharacters.java

--- java/159_LongestSubstringWithAtMostTwoDistinctCharacters/LongestSubstringWithAtMostTwoDistinctCharacters.java
+++ java/159_LongestSubstringWithAtMostTwoDistinctCharacters/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -6,7 +6,7 @@ class Solution {
         int ret = 0;
         for (int i = 0; i < s.length(); i++) {
             hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
-            while (j < s.length() && hm.size() > 2) {
+            while (j <= i && hm.size() > 2) {
                 hm.put(s.charAt(j), hm.get(s.charAt(j)) - 1);
                 if (hm.get(s.charAt(j)) == 0) hm.remove(s.charAt(j));
                 j++;
