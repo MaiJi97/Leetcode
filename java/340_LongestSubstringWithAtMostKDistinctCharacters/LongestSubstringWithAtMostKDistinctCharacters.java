bd7a7d4020abd0a8f13614b607ff900c3ec7a30c
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-04 10:50:33 +0800

    Update LongestSubstringWithAtMostKDistinctCharacters.java

--- java/340_LongestSubstringWithAtMostKDistinctCharacters/LongestSubstringWithAtMostKDistinctCharacters.java
+++ java/340_LongestSubstringWithAtMostKDistinctCharacters/LongestSubstringWithAtMostKDistinctCharacters.java
@@ -6,7 +6,7 @@ class Solution {
         int ret = 0;
         for (int i = 0; i < s.length(); i++) {
             hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
-            while (j < s.length() && hm.size() > k) {
+            while (j <= i && hm.size() > k) {
                 hm.put(s.charAt(j), hm.get(s.charAt(j)) - 1);
                 if (hm.get(s.charAt(j)) == 0) hm.remove(s.charAt(j));
                 j++;
