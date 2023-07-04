5433321ef2890e0daa3f617274f18048c4808a54
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-04 13:17:34 +0800

    Update QuickSort.java

--- java/912_SortAnArray/QuickSort.java
+++ java/912_SortAnArray/QuickSort.java
@@ -1,1 +1,37 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        quickSort(nums, 0, nums.length-1);
+        return nums;
+    }
 
+    private void quickSort(int[] nums, int left, int right) {
+        if (left >= right) return;
+        int pivot = partition(nums, left, right);
+        quickSort(nums, left, pivot - 1);
+        quickSort(nums, pivot + 1, right);
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int pivot = nums[right];
+        int start = left;
+        int end = right - 1;
+        while (start <= end) {
+            if (nums[start] <= pivot) start++;
+            else if (nums[end] > pivot) end--;
+            else {
+                swap(nums, start, end);
+                start++;
+                end--;
+            }
+        }
+        swap(nums, start, right);
+        return start;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
