71ab4b7b112458b0ff6decf3e2d6b42493e74810
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-11 19:37:01 +0800

    Create SortCharacterByFrequency.java

---
+++ java/451_SortCharactersByFrequency/SortCharacterByFrequency.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        List<Character>[] bucketSort = new List[s.length() + 1];
+        for (char key : hm.keySet()) {
+            int frequency = hm.get(key);
+            if (bucketSort[frequency] == null) {
+                List<Character> list = new ArrayList<>();
+                bucketSort[frequency] = list;
+            }
+            bucketSort[frequency].add(key);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length(); i >= 0; i--) {
+            if (bucketSort[i] != null) {
+                for (int j = 0; j < bucketSort[i].size(); j++) {
+                    for (int k = 0; k < i; k++) {
+                        sb.append(bucketSort[i].get(j));
+                    }
+                }
+            }
+        }
+        return sb.toString();
+      
+    }
+}
