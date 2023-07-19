e7f0d9ce3064a107d40d9486de037c98d98969b3
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-19 15:57:40 +0800

    Update GenerateParentheses.java

--- java/22_GenerateParentheses/GenerateParentheses.java
+++ java/22_GenerateParentheses/GenerateParentheses.java
@@ -1,16 +1,22 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ret = new ArrayList<>();
-        dfs(n, new String(), 0, 0, ret);
+        dfs(n, new StringBuilder(), 0, 0, ret);
         return ret;
     }
 
-    private void dfs(int n, String current, int open, int close, List<String> ret) {
+    private void dfs(int n, StringBuilder current, int open, int close, List<String> ret) {
         if (current.length() == n * 2) {
-            ret.add(current);
+            ret.add(current.toString());
             return;
         }
-        if (open < n) dfs(n, current + '(', open + 1, close, ret);
-        if (open > close) dfs(n, current + ')', open, close + 1, ret);
+        if (open < n) {
+            dfs(n, current.append('('), open + 1, close, ret);
+            current.deleteCharAt(current.length() - 1);
+        }
+        if (open > close) {
+            dfs(n,current.append(')'), open, close + 1, ret);
+            current.deleteCharAt(current.length() - 1);
+        }
     }
 }
