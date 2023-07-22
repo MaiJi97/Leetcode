2f3c23e13d44581cdf2dbc91375cc5cdab38782a
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-22 09:06:10 +0800

    Update PeakIndexInMountainArray2.java

--- java/852_PeakIndexInMountainArray/PeakIndexInMountainArray2.java
+++ java/852_PeakIndexInMountainArray/PeakIndexInMountainArray2.java
@@ -2,13 +2,13 @@ class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         int left = 0; 
         int right = arr.length - 1;
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] > arr[mid + 1]) {
-                right = mid;
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
             }
             else {
-                left = mid + 1;
+                right = mid - 1;
             }
         }
         return left;
