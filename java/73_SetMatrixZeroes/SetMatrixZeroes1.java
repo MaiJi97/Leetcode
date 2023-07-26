0a8b872f4ed565ac75de68c904e0dbd839210644
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-26 14:08:24 +0800

    Create SetMatrixZeroes1.java

---
+++ java/73_SetMatrixZeroes/SetMatrixZeroes1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        HashSet<Integer> rowSet = new HashSet<>();
+        HashSet<Integer> colSet = new HashSet<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    rowSet.add(i);
+                    colSet.add(j);
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (rowSet.contains(i) || colSet.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
