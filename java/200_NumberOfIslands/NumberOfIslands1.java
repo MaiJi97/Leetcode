fb0f8008d153c305ddbf68f91c9d13f70bf7347a
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-27 18:24:21 +0800

    Update NumberOfIslands1.java

--- java/200_NumberOfIslands/NumberOfIslands1.java
+++ java/200_NumberOfIslands/NumberOfIslands1.java
@@ -1,16 +1,30 @@
 class Solution {
+    
+    class Coordinate {
+        int x, y;
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int[] deltaX = {0, 1, 0, -1};
+    int[] deltaY = {1, 0, -1, 0};
+        
     public int numIslands(char[][] grid) {
         int count = 0;
         if (grid == null || grid.length == 0) {
             return 0;
         }
         int m = grid.length;
-        int n = grid[0].length;
+        int n = grid[0].length;    
         boolean[][] visited = new boolean[m][n];
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (visited[i][j] == false && grid[i][j] == '1') {
-                    dfs(i, j, grid, visited);
+                    bfs(i, j, grid, visited);
                     count++;
                 }
             }
@@ -18,14 +32,21 @@ class Solution {
         return count;
     }
     
-    private void dfs(int i, int j, char[][] grid, boolean[][] visited) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0' || visited[i][j] == true) {
-            return;
+    private void bfs(int i, int j, char[][] grid, boolean[][] visited) {
+        Queue<Coordinate> q = new ArrayDeque<>();
+        q.offer(new Coordinate(i, j));
+        visited[0][0] = true;
+        while (!q.isEmpty()) {
+            Coordinate current = q.poll();
+            for (int[] direction : directions) {
+                int newX = current.x + direction[0];
+                int newY = current.y + direction[1];
+                if (newX < 0 || newX >= grid.length || newY < 0 || newY >= grid[0].length || grid[newX][newY] == '0' || visited[newX][newY] == true) {
+                    continue;
+                }
+                q.offer(new Coordinate(newX, newY));
+                visited[newX][newY] = true;
+            }
         }
-        visited[i][j] = true;
-        dfs(i + 1, j, grid, visited);
-        dfs(i, j + 1, grid, visited);
-        dfs(i - 1, j, grid, visited);
-        dfs(i, j - 1, grid, visited);
     }
 }
