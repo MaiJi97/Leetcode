65b501f1a235b5685425da8bc3d5710b7c07df9e
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-27 18:37:09 +0800

    Update NumberOfProvinces2.java

--- java/547_NumberOfProvinces/NumberOfProvinces2.java
+++ java/547_NumberOfProvinces/NumberOfProvinces2.java
@@ -1,27 +1,21 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int count = 0;
         boolean[] visited = new boolean[isConnected.length];
+        int count = 0;
         for (int i = 0; i < isConnected.length; i++) {
-            if (visited[i] == false) {
-                bfs(i, isConnected, visited);
+            if (!visited[i]) {
+                dfs(i, isConnected, visited);
                 count++;
             }
         }
         return count;
     }
     
-    private void bfs(int i, int[][] isConnected, boolean[] visited) {
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(i);
+    private void dfs(int i, int[][] isConnected, boolean[] visited) {
         visited[i] = true;
-        while (!q.isEmpty()) {
-            Integer current = q.poll();
-            for (int j = 0; j < isConnected[i].length; j++) {
-                if (isConnected[current][j] == 1 && visited[j] == false) {
-                    q.offer(j);
-                    visited[j] = true;
-                }
+        for (int j = 0; j < isConnected[0].length; j++) {
+            if (isConnected[i][j] == 1 && visited[j] == false) {
+                dfs(j, isConnected, visited);
             }
         }
     }
