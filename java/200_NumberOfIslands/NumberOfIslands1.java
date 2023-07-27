5776b3093b55d25ab12516d9760f261d91dbf685
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-07-27 18:30:18 +0800

    Update NumberOfIslands1.java

--- java/200_NumberOfIslands/NumberOfIslands1.java
+++ java/200_NumberOfIslands/NumberOfIslands1.java
@@ -8,7 +8,6 @@ class Solution {
         }
     }
     
-    int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     int[] deltaX = {0, 1, 0, -1};
     int[] deltaY = {1, 0, -1, 0};
         
@@ -33,18 +32,18 @@ class Solution {
     }
     
     private void bfs(int i, int j, char[][] grid, boolean[][] visited) {
-        Queue<Coordinate> q = new ArrayDeque<>();
-        q.offer(new Coordinate(i, j));
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{i, j});
         visited[0][0] = true;
         while (!q.isEmpty()) {
-            Coordinate current = q.poll();
-            for (int[] direction : directions) {
-                int newX = current.x + direction[0];
-                int newY = current.y + direction[1];
+            int[] current = q.poll();
+            for (int direction = 0; direction < 4; direction++) {
+                int newX = current[0] + deltaX[direction];
+                int newY = current[1] + deltaY[direction];
                 if (newX < 0 || newX >= grid.length || newY < 0 || newY >= grid[0].length || grid[newX][newY] == '0' || visited[newX][newY] == true) {
                     continue;
                 }
-                q.offer(new Coordinate(newX, newY));
+                q.offer(new int[]{newX, newY});
                 visited[newX][newY] = true;
             }
         }
