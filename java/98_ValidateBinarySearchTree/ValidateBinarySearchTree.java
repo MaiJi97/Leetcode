36cb6304e3f8e226713dc49a72ff96052044599d
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-01 17:16:39 +0800

    Update ValidateBinarySearchTree.java

--- java/98_ValidateBinarySearchTree/ValidateBinarySearchTree.java
+++ java/98_ValidateBinarySearchTree/ValidateBinarySearchTree.java
@@ -1,16 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return valid(root, null, null);
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    public boolean valid(TreeNode root, Integer low, Integer high) {
+    public boolean valid(TreeNode root, long low, long high) {
         if (root == null) {
             return true;
         }
-        else if (low != null && root.val <= low) {
-            return false;
-        }
-        else if (high != null && root.val >= high) {
+        else if (root.val <= low || root.val >= high) {
             return false;
         }
         return valid(root.left, low, root.val) && valid(root.right, root.val, high);
