240ca68d48254898a6d774bd05deef9ab1b31bb7
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-03 19:09:02 +0800

    Update GraphValidTree.java

--- java/261_GraphValidTree/GraphValidTree.java
+++ java/261_GraphValidTree/GraphValidTree.java
@@ -3,14 +3,13 @@ class Solution {
         if (edges.length != (n-1)) return false;
 
         HashMap<Integer, List<Integer>> hm = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            hm.put(i, new ArrayList<>());
+        }
+
         for (int i = 0; i < edges.length; i++) {
             int[] edge = edges[i];
-            if (!hm.containsKey(edge[0])) {
-                hm.put(edge[0], new ArrayList<>());
-            }
-            if (!hm.containsKey(edge[1])) {
-                hm.put(edge[1], new ArrayList<>());
-            }
             hm.get(edge[0]).add(edge[1]);
             hm.get(edge[1]).add(edge[0]);
         }
@@ -25,13 +24,11 @@ class Solution {
 
     private void dfs(HashMap<Integer, List<Integer>> hm, boolean[] visited, int i) {
         visited[i] = true;
-        if (hm.containsKey(i)) {
-            for (int neighbor : hm.get(i)) {
-                if (visited[neighbor]) {
-                    continue;
-                }
-                dfs(hm, visited, neighbor);
+        for (int neighbor : hm.get(i)) {
+            if (visited[neighbor]) {
+                continue;
             }
+            dfs(hm, visited, neighbor);
         }
     }
 }
