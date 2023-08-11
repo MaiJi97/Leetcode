8895977e05bf74cce30f4947c89c96b113539044
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-11 14:45:15 +0800

    Update GasStation.java

--- java/134_GasStation/GasStation.java
+++ java/134_GasStation/GasStation.java
@@ -1,12 +1,14 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        // gas[i] - cost[i]: [-2, -2, -2, 3, 3]
+        // gain[i] = gas[i] - cost[i]: [-2, -2, -2, 3, 3]
         int totalGain = 0; 
         int currentGain = 0; 
         int ret = 0;
         for (int i = 0; i < gas.length; i++) {
             totalGain  += gas[i] - cost[i];
             currentGain += gas[i] - cost[i];
+            // If we meet a "valley", start over from the next station
+            // with 0 initial gas.
             if (currentGain < 0) {
                 ret = i + 1;
                 currentGain = 0;
