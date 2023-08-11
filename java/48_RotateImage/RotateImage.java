c248685070e8abd5e77e388adafc9d842df17070
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-11 15:09:40 +0800

    Update RotateImage.java

--- java/48_RotateImage/RotateImage.java
+++ java/48_RotateImage/RotateImage.java
@@ -7,7 +7,7 @@ class Solution {
                 matrix[j][i] = temp;
             }
         }
-        for (int i = 0; i < matrix[0].length; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length / 2; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[i][matrix[0].length - 1 - j];
