f36a69d4852859dfad65b0b5eeeb4c95512afb9d
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-12 18:55:13 +0800

    Update ThreeSum.java

--- java/15_ThreeSum/ThreeSum.java
+++ java/15_ThreeSum/ThreeSum.java
@@ -6,7 +6,8 @@ class Solution {
             int left = i + 1;
             int right = nums.length - 1;
             while (left < right) {
-                if (nums[i] + nums[left] + nums[right] == 0) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
                     List<Integer> temp = List.of(nums[i], nums[left], nums[right]);
                     ret.add(temp);
                     while (left + 1 < right && nums[left] == nums[left + 1]) {
@@ -18,12 +19,13 @@ class Solution {
                     left++;
                     right--;
                 }
-                else if (nums[i] + nums[left] + nums[right] < 0) left++;
+                else if (sum < 0) left++;
                 else right--;
             }
             while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
                 i++;
             }
+
         }
         return ret;
     }
