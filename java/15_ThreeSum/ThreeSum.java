74fa46dd012178e22599f505230a0980b20fc20f
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-12 18:47:38 +0800

    Update ThreeSum.java

--- java/15_ThreeSum/ThreeSum.java
+++ java/15_ThreeSum/ThreeSum.java
@@ -1,15 +1,15 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ret = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length-1; i++) {
-            int target = 0 - nums[i];
+        List<List<Integer>> ret = new ArrayList<>();
+        for (int i = 0; i < nums.length - 1; i++) {
             int left = i + 1;
             int right = nums.length - 1;
-            while (left <= right) {
-                if (nums[left] + nums[right] == target) {
-                    ret.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while (left + 1 < nums.length && nums[left + 1] == nums[left]) {
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] == 0) {
+                    List<Integer> temp = List.of(nums[i], nums[left], nums[right]);
+                    ret.add(temp);
+                    while (left + 1 < right && nums[left] == nums[left + 1]) {
                         left++;
                     }
                     while (right - 1 > left && nums[right - 1] == nums[right]) {
@@ -18,12 +18,8 @@ class Solution {
                     left++;
                     right--;
                 }
-                else if (nums[left] + nums[right] < target) {
-                    left++;
-                }
-                else {
-                    right--;
-                }
+                else if (nums[i] + nums[left] + nums[right] < 0) left++;
+                else right--;
             }
             while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
                 i++;
