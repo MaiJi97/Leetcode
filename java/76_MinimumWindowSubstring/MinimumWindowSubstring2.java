d3d27c11d4f97af4eaee5dd625d64d6a7016244a
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-15 22:00:37 +0800

    Update MinimumWindowSubstring2.java

--- java/76_MinimumWindowSubstring/MinimumWindowSubstring2.java
+++ java/76_MinimumWindowSubstring/MinimumWindowSubstring2.java
@@ -1,37 +1,30 @@
 class Solution {
     public String minWindow(String s, String t) {
-        // Example: t = "ABC", s = "ADOBECODEBANC"
-        int[] map = new int[128];
-        for (char c : t.toCharArray()) {
-            map[c]++;
-        }
-        //          A B C 
-        // count = [1,1,1,0,0,0...]
-        
-        int begin = 0; 
+        int[] frequencies = new int[128]; // since strings can contain both uppercase or lowercase letters
+        int length = Integer.MAX_VALUE;
         int count = t.length();
-        int len = Integer.MAX_VALUE;
+        int start = 0;
+        for (int i = 0; i < t.length(); i++) {
+            frequencies[t.charAt(i)]++;
+        }
         int j = 0;
-        
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            map[c]--;
-            if (map[c] >= 0) {
-                count--;
-            }
-            while (count == 0) { // 在t中出现的所有char在当前部分的s中都出现且出现了一次（全部抵消）
-                char lc = s.charAt(j);
-                map[lc]++; // 准备滑动left pointer，对应的map[c]和count都要++
-                if (map[lc] > 0) {
-                    if (i - j + 1 < len) {
-                        begin = j;
-                        len = i - j + 1;
-                    }
-                    count++;
+            frequencies[s.charAt(i)]--;
+
+            if (frequencies[s.charAt(i)] >= 0) count--; // 有效的抵消
+            
+            while (count == 0) { // 通过count来代替每次checkValid
+                if (i - j + 1 < length) {
+                    length = i - j + 1;
+                    start = j;
                 }
-                j++; // 滑动left pointer
+                if (frequencies[s.charAt(j)] >= 0) count++;
+                frequencies[s.charAt(j)]++;  
+                j++;
+
             }
         }
-        return len == Integer.MAX_VALUE ? "" : s.substring(begin, begin + len);
+        return length == Integer.MAX_VALUE ? "" : s.substring(start, start + length);
     }
+
 }
