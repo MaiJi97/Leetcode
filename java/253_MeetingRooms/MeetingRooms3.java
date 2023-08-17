9f735b3687c07c254af6e0c70004b26831280cdc
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-17 16:02:59 +0800

    Update MeetingRooms3.java

--- java/253_MeetingRooms/MeetingRooms3.java
+++ java/253_MeetingRooms/MeetingRooms3.java
@@ -1,14 +1,20 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]); // sort by start time
-        Queue<Integer> q = new PriorityQueue<Integer>((a, b) -> a - b); // sort by end time
-        q.offer(intervals[0][1]);
-        for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] >= q.peek()) { // a new start is later than the earliest end time
-                q.poll();
-            }
-            q.offer(intervals[i][1]);
+        int[] starts = new int[intervals.length];
+        int[] ends = new int[intervals.length];
+        for (int i = 0; i < intervals.length; i++) {
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
         }
-        return q.size();
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        
+        int ret = 1;
+        int j = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            if (starts[i] < ends[j]) ret++;
+            else j++;
+        }
+        return ret;
     }
 }
