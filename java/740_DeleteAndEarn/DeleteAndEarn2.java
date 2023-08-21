c176fd3ff8d00c4a6c02c43126f98dbbaf19a9f2
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-21 08:06:57 -0500

    Update DeleteAndEarn2.java

--- java/740_DeleteAndEarn/DeleteAndEarn2.java
+++ java/740_DeleteAndEarn/DeleteAndEarn2.java
@@ -4,14 +4,16 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             max = Math.max(max, nums[i]);
         }
-        int[] dp = new int[max + 1];
-
-        for (int i = 0; i < nums.length; i++) { // 类似bucket sort
-            dp[nums[i]] += nums[i]; 
+        int[] bucketSort = new int[max + 1];
+        for (int i = 0; i < nums.length; i++) {
+            bucketSort[nums[i]]++;
         }
 
+        int[] dp = new int[max + 1];
+        dp[0] = 0;
+        dp[1] = bucketSort[1] * 1;
         for (int i = 2; i < max + 1; i++) {
-            dp[i] = Math.max(dp[i - 2] + dp[i] , dp[i - 1]);
+            dp[i] = Math.max(dp[i - 2] + i * bucketSort[i], dp[i - 1]);
         }
         return dp[max];
     }
