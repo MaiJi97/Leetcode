338b31c005aa577f0bf55f05f3015f028db21b47
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-24 11:15:53 -0500

    Update MeetingRooms4.java

--- java/253_MeetingRooms/MeetingRooms4.java
+++ java/253_MeetingRooms/MeetingRooms4.java
@@ -2,12 +2,13 @@ class Solution {
     public int minMeetingRooms(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         Queue<Integer> pq = new PriorityQueue<>((a, b) -> a - b);
-        pq.offer(intervals[0][1]);
+        for (int i = 0; i < intervals.length; i++) {
+            pq.offer(intervals[i][1]);
+        }
         int ret = 1;
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] < pq.peek()) ret++;
             else pq.poll();
-            pq.offer(intervals[i][1]);
         }
         return ret;
     }
