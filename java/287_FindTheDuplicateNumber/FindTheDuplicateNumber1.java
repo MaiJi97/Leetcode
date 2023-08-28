771635ef55433c2085b12e790080ba3bb31e5f27
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-28 18:21:42 -0500

    Update FindTheDuplicateNumber1.java

--- java/287_FindTheDuplicateNumber/FindTheDuplicateNumber1.java
+++ java/287_FindTheDuplicateNumber/FindTheDuplicateNumber1.java
@@ -6,16 +6,10 @@ class Solution {
             int mid = left + (right - left) / 2;
             int count = 0;
             for (int i = 0; i < nums.length; i++) {
-                if (nums[i] <= mid) {
-                    count++;
-                }
+                if (nums[i] <= mid) count++;
             }
-            if (count > mid) {
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
-            }
+            if (count <= mid) left = mid + 1;
+            else right = mid - 1;
         }
         return left;
     }
