0acf50134ac301cf2939217b2c8246a0de9c4571
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-27 20:30:15 -0500

    Update RemoveCoveredIntervals.java

--- java/1288_RemoveCoveredIntervals/RemoveCoveredIntervals.java
+++ java/1288_RemoveCoveredIntervals/RemoveCoveredIntervals.java
@@ -1,12 +1,15 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
-        int prev = 0;
         int count = 1;
-        for (int i = 1; i < intervals.length; i++) {
-            if (intervals[prev][1] < intervals[i][1]) {
+        int[] newInterval = intervals[0];
+        for (int[] interval : intervals) {
+            if (interval[1] <= newInterval[1]) {
+                continue;
+            }
+            else {
                 count++;
-                prev = i;
+                newInterval = interval;
             }
         }
         return count;
