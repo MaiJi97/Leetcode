5f1a7d800c3ae9c8559d681b4eaa9ec100f6f0e7
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-29 11:00:40 -0500

    Update FindMinimumInRotatedSortedArray.java

--- java/154_FindMinimumInRotatedSortedArray/FindMinimumInRotatedSortedArray.java
+++ java/154_FindMinimumInRotatedSortedArray/FindMinimumInRotatedSortedArray.java
@@ -4,15 +4,9 @@ class Solution {
         int right = nums.length - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;
-            }
-            else if (nums[mid] < nums[left]) {
-                right = mid;
-            }
-            else {
-                right--;
-            }
+            if (nums[mid] > nums[right]) left = mid + 1;
+            else if (nums[mid] < nums[right]) right = mid;
+            else right--;
         }
         return nums[left];
     }
