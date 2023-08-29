b3b184b7d88a0815be90e88cecf15623884118a8
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-29 14:58:21 -0500

    Update MinimumTimeToCompleteTrips.java

--- java/2187_MinimumTimeToCompleteTrips/MinimumTimeToCompleteTrips.java
+++ java/2187_MinimumTimeToCompleteTrips/MinimumTimeToCompleteTrips.java
@@ -1,16 +1,20 @@
 class Solution {
     public long minimumTime(int[] time, int totalTrips) {
-        long left = 1;
+        long left = 0;
         int max = 0;
         for (int i = 0; i < time.length; i++) {
             max = Math.max(max, time[i]);
         }
         long right = (long) totalTrips * max;
 
-        while (left < right) {
+        int ret = 0;
+
+        while (left <= right) {
             long mid = left + (right - left) / 2;
-            if (numTrips(time, mid) >= totalTrips) right = mid;
-            else left = mid + 1;
+            if (numTrips(time, mid) < totalTrips) left = mid + 1;
+            else {
+                right = mid - 1;
+            }
         }
         return left;
     }
