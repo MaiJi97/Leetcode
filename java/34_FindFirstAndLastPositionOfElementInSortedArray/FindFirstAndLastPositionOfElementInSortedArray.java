1a583b176bb97003913514304aca89d91fe7f514
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-28 19:06:48 -0500

    Update FindFirstAndLastPositionOfElementInSortedArray.java

--- java/34_FindFirstAndLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArray.java
+++ java/34_FindFirstAndLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -14,11 +14,11 @@ class Solution {
         
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] >= target) {
-                right = mid - 1;
+            if (nums[mid] < target) {
+                left = mid + 1;
             }
             else {
-                left = mid + 1;
+                right = mid - 1;
             }
             
             if (nums[mid] == target) {
@@ -35,11 +35,11 @@ class Solution {
         
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
-                left = mid + 1;
+            if (nums[mid] > target) {
+                right = mid - 1;
             }
             else {
-                right = mid - 1;
+                left = mid + 1;
             }
             
             if (nums[mid] == target) {
