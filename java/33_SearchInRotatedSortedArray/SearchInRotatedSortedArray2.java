d99cf1ce72387862c8314d0903e327fe7a8246c2
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-08-28 21:43:33 -0500

    Update SearchInRotatedSortedArray2.java

--- java/33_SearchInRotatedSortedArray/SearchInRotatedSortedArray2.java
+++ java/33_SearchInRotatedSortedArray/SearchInRotatedSortedArray2.java
@@ -5,26 +5,26 @@ class Solution {
         // [7,8,0,1,2,3,4,5,6]
     
         int left = 0;
-        int right = nums.length;
-        while (left < right) {
+        int right = nums.length - 1;
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-            else if (nums[mid] > nums[left]) {
-                if (target <= nums[mid] && target >= nums[left]) { // target between left and mid, including mid
-                    right = mid;
+            else if (nums[mid] >= nums[left]) {
+                if (target < nums[mid] && target >= nums[left]) { // target between left and mid, including mid
+                    right = mid - 1;
                 }
                 else {
                     left = mid + 1;
                 }
             }
             else {
-                if (target > nums[mid] && target <= nums[right - 1]) { // target between mid and right-1, excluding mid
+                if (target > nums[mid] && target <= nums[right]) { // target between mid and right-1, excluding mid
                     left = mid + 1;
                 }
                 else {
-                    right = mid;
+                    right = mid - 1;
                 }
             }
         }
