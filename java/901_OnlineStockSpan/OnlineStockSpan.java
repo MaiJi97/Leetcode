0c1edf83539821f5d5b7bf53d881feab8b5f0eae
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-09-10 16:45:53 -0500

    Create OnlineStockSpan.java

---
+++ java/901_OnlineStockSpan/OnlineStockSpan.java
@@ -0,0 +1,23 @@
+class StockSpanner {
+    
+    public Stack<int[]> stack;
+
+    public StockSpanner() {
+        this.stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int count = 1;
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            count += stack.pop()[1];
+        } 
+        stack.push(new int[]{price, count});
+        return count;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
