0f897c8bbd08c97d8e139187efcf860ba22838a9
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-09-11 21:31:44 -0500

    Update ShortestSubarrayWithSumAtLeastK.java

--- java/862_ShortestSubarrayWithSumAtLeastK/ShortestSubarrayWithSumAtLeastK.java
+++ java/862_ShortestSubarrayWithSumAtLeastK/ShortestSubarrayWithSumAtLeastK.java
@@ -8,6 +8,10 @@ class Solution {
         }
         for (int i = 0; i < nums.length + 1; i++) {
             while (!q.isEmpty() && prefixSum[i] - prefixSum[q.peekFirst()] >= k) ret = Math.min(ret, i - q.pollFirst());
+
+            // This is a bit tricky. Removing equal or larger sums at previous indices is safe since they won't contribute to valid answers. 
+			// Let's say we have two indexes i1 and i2 , where i1 < i2 and sums[i1] = sums[i2].
+			// Our current index is j. Then the shortest subarray length can only be j - i2. Same thing if sums[i1] > sums[i2].
             while (!q.isEmpty() && prefixSum[q.peekLast()] >= prefixSum[i]) q.pollLast();
             q.offerLast(i);
         }
