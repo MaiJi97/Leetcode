2d159b4bff4b443d547105ff2fb6155e85329c61
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-09-15 13:42:56 -0500

    Create NumberOf1Bits.java

---
+++ java/191_NumberOf1Bits/NumberOf1Bits.java
@@ -0,0 +1,12 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        int mask = 1;
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) != 0) count++;
+            mask = mask << 1; // 1 to 10 to 100...
+        }
+        return count;
+    }
+}
