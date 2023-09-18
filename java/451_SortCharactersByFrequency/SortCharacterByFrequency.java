4db603cb204084436e1f47072086609a4684ee6a
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-09-18 12:52:30 -0500

    Update SortCharacterByFrequency.java

--- java/451_SortCharactersByFrequency/SortCharacterByFrequency.java
+++ java/451_SortCharactersByFrequency/SortCharacterByFrequency.java
@@ -8,7 +8,7 @@ class Solution {
         for (char key : hm.keySet()) {
             int frequency = hm.get(key);
             if (bucketSort[frequency] == null) {
-                List<Character> list = new ArrayList<>(); // need this step else we'll get type error
+                List<Character> list = new ArrayList<>(); 
                 bucketSort[frequency] = list;
             }
             bucketSort[frequency].add(key);
