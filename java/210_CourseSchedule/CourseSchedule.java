fc99f76d2e10961d885a3adb4a85507977f5d933
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-09-19 20:16:13 -0500

    Create CourseSchedule.java

---
+++ java/210_CourseSchedule/CourseSchedule.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        int[] inDegree = new int[numCourses];
+        int[] order = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) { 
+            graph.put(i, new ArrayList<Integer>());
+        }
+        
+        for (int[] pre : prerequisites) { // construct graph, count inDegree of each node
+            int from = pre[1];
+            int to = pre[0];
+            graph.get(from).add(to);
+            inDegree[to]++;
+        }
+        
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i); // 将每个in degree为0的点放入queue中作为起始点
+            }
+        }
+
+        int count = 0;
+        
+        while (!queue.isEmpty()) {
+            int current = queue.poll(); // 拿出一个点并去掉所有连边，in degree - 1
+            order[count] = current;
+            count++;
+            for (Integer neighbor : graph.get(current)) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) { // 一旦发现新的in degree为0的点，丢入queue中
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        return count == numCourses ? order : new int[0];   
+    }
+}
