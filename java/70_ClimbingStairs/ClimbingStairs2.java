dcb3ea74bdb17ae75dcc3dda8a5d95833c5ef11f
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-10-09 15:11:52 -0500

    Update ClimbingStairs2.java

--- java/70_ClimbingStairs/ClimbingStairs2.java
+++ java/70_ClimbingStairs/ClimbingStairs2.java
@@ -1,13 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n == 1) return 1;
         int[] memo = new int[n + 1];
-        memo[1] = 1;
-        memo[2] = 2;
         return dfs(n, memo);
     }
 
     public int dfs(int n, int[] memo) {
+        if (n == 1) return memo[n] = 1;
+        if (n == 2) return memo[n] = 2;
         if (memo[n] != 0) return memo[n];
         int current = dfs(n - 1, memo) + dfs(n - 2, memo);
         return memo[n] = current;
