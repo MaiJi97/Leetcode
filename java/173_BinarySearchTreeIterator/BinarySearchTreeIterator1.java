c0bc0c25d7611bfd6cf7607fd97c27fcdb712b0a
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-10-28 15:26:59 -0500

    Update and rename BinarySearchTreeIterator.java to BinarySearchTreeIterator1.java

---
+++ java/173_BinarySearchTreeIterator/BinarySearchTreeIterator1.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+
+    List<Integer> list;
+    int pointer;
+
+    public BSTIterator(TreeNode root) {
+        this.list = new ArrayList<>();
+        dfs(root, list);
+        this.pointer = 0;
+    }
+
+    public void dfs(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        dfs(root.left, list);
+        list.add(root.val);
+        dfs(root.right, list);
+    }
+    
+    public int next() {
+        int ret = list.get(pointer); 
+        pointer++;
+        return ret;
+    }
+    
+    public boolean hasNext() {
+        return pointer <= list.size() - 1;
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
