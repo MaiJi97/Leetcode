ad02cb76518fc25c8bf495c8514d89d81171fc89
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-11-02 16:16:12 -0500

    Update LRUCache.java

--- java/146_LRUCache/LRUCache.java
+++ java/146_LRUCache/LRUCache.java
@@ -1,78 +1,70 @@
+class Node {
+    int key;
+    int value;
+    Node next;
+    Node prev;
+
+    public Node (int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
 class LRUCache {
 
-    class Node {
-        int key;
-        int value;
-        Node next;
-        Node prev;
-        public Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-
     int capacity;
+    int size;
     HashMap<Integer, Node> hm;
     Node head;
     Node tail;
-    int size;
 
+   
     public LRUCache(int capacity) {
         this.capacity = capacity;
         this.size = 0;
         this.hm = new HashMap<>();
-        head = new Node(0, 0);
-        tail = new Node(0, 0);
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0);
         head.next = tail;
         tail.prev = head;
     }
-    
+
     public int get(int key) {
-        if (!hm.containsKey(key)) {
-            return -1;
-        }
-        else {
-            Node node = hm.get(key);
-            remove(key);
-            addHead(key, node.value);
-            return node.value;
-        }
+        if (!hm.containsKey(key)) return -1;
+        int value = hm.get(key).value;
+        remove(key);
+        addHead(key, value);
+        return value;
     }
-    
+
     public void put(int key, int value) {
-        if (hm.containsKey(key)) {
-            remove(key);
-            addHead(key, value);
-        }
-        else {
-            addHead(key, value);
-        }
+        if (hm.containsKey(key)) remove(key);
+        addHead(key, value);
     }
 
     public void remove(int key) {
         Node current = hm.get(key);
-        Node next = current.next;
         Node prev = current.prev;
+        Node next = current.next;
         prev.next = next;
         next.prev = prev;
-        size--;
         hm.remove(key);
+        size--;
     }
 
     public void addHead(int key, int value) {
+        if (size == capacity) remove(tail.prev.key);
         Node node = new Node(key, value);
         Node next = head.next;
         head.next = node;
         node.prev = head;
         node.next = next;
         next.prev = node;
-        hm.put(key, node);
         size++;
-        if (size > capacity) {
-            Node preTail = tail.prev;
-            remove(preTail.key);
-        }
+        hm.put(key, node);
     }
+
+    
 }
 
 /**
