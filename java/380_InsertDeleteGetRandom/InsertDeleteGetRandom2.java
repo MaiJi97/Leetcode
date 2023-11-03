151efa20196a543531fa483603f3c47798c58099
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-11-03 18:37:23 -0500

    Update InsertDeleteGetRandom2.java

--- java/380_InsertDeleteGetRandom/InsertDeleteGetRandom2.java
+++ java/380_InsertDeleteGetRandom/InsertDeleteGetRandom2.java
@@ -1,37 +1,41 @@
 class RandomizedSet {
-    ArrayList<Integer> list;
-    Map<Integer, Integer> hm; // <element, index>
+
+    ArrayList<Integer> list; 
+    HashMap<Integer, Integer> hm; // <element, index>
     java.util.Random rand = new java.util.Random();
 
     public RandomizedSet() {
-        list = new ArrayList<>();
-        hm = new HashMap<>();
+        this.list = new ArrayList<>();
+        this.hm = new HashMap<>(); 
     }
     
     public boolean insert(int val) {
-        if (hm.containsKey(val)) {
-            return false;
-        }
-        list.add(val);
-        hm.put(val, list.size()-1);
-        return true;
+       if (hm.containsKey(val)) return false;
+       list.add(val);
+       hm.put(val, list.size() - 1);
+       return true;
     }
     
     public boolean remove(int val) {
-        if (!hm.containsKey(val)) {
-            return false;
-        }
-        int index = hm.get(val);
-        list.set(index, list.get(list.size()-1));
-        hm.put(list.get(list.size()-1), index);
-        list.set(list.size()-1, val);
-        list.remove(list.size()-1);
+        if (!hm.containsKey(val)) return false;
+        swap(hm.get(val), list.size() - 1);
+        list.remove(list.size() - 1);
         hm.remove(val);
         return true;
     }
     
     public int getRandom() {
-        return list.get(rand.nextInt(list.size()));
+        int index = rand.nextInt(list.size());
+        return list.get(index);
+    }
+
+    public void swap(int indexToRemove, int indexLast) {
+        int elementToRemove = list.get(indexToRemove);
+        int elementLast = list.get(indexLast);
+        list.set(indexToRemove, elementLast);
+        list.set(indexLast, elementToRemove);
+        hm.put(elementToRemove, indexLast);
+        hm.put(elementLast, indexToRemove);
     }
 }
 
