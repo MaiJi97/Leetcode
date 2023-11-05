cfe936cda057d053e1209e7e90ef5ed17d956af8
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-11-05 17:50:38 -0600

    Update ThreeSumClosest.java

--- java/16_ThreeSumClosest/ThreeSumClosest.java
+++ java/16_ThreeSumClosest/ThreeSumClosest.java
@@ -1,26 +1,27 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int result = nums[0] + nums[1] + nums[nums.length - 1];
         Arrays.sort(nums);
-        for (int i=0 ; i < nums.length - 1 ; i++) {    
-            int left = i + 1 , right = nums.length - 1 ;          
-            while (left <= right) {
+        int ret = nums[0] + nums[1] + nums[nums.length - 1];
+        for (int i = 0; i < nums.length - 1; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                
                 if (sum == target) {
-                    return sum;
-                }   
-                else if (sum > target) {
-                    right--;
+                    ret = sum;
+                    break;
                 }
-                else {        
+                else if (sum < target) {
                     left++;
                 }
-                if (Math.abs(sum - target) < Math.abs(result - target)) {
-                    result = sum ;
+                else {
+                    right--;              
+                }
+                if (Math.abs(sum - target) < Math.abs(ret - target)) {
+                    ret = sum;
                 }
             }
         }
-        return result ;
+        return ret;
     }
 }
