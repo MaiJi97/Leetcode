aba3a8205fb289687d70197f4d91e75a6ed239d1
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-11-05 19:16:00 -0600

    Update ThreeSumSmaller.java

--- java/259_ThreeSumSmaller/ThreeSumSmaller.java
+++ java/259_ThreeSumSmaller/ThreeSumSmaller.java
@@ -2,16 +2,16 @@ class Solution {
     public int threeSumSmaller(int[] nums, int target) {
         Arrays.sort(nums);
         int ret = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             int left = i + 1;
             int right = nums.length - 1;
-            while (left <= right) {
+            while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum < target) {
                     ret += (right - left);
                     left++;
                 }
-                else if (sum >= target) right--;
+                else right--;
             }
         }
         return ret;
