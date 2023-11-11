3551aa96da5df203360f8378187290724f9dec76
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-11-11 17:07:45 -0600

    Create 443_StringCompression1.java

---
+++ java/443_StringCompression/443_StringCompression1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int compress(char[] chars) {
+        int j = 0;
+        char current = chars[0];
+        int count = 1;
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] != chars[j]) {
+                if (count >= 2) {
+                    String countString = String.valueOf(count);
+                    for (char c : countString.toCharArray()) {
+                        j++;
+                        chars[j] = c;
+                    }
+                }
+                j++;
+                chars[j] = chars[i]; 
+                count = 1;
+            }
+            else count++;
+        }
+        if (count >= 2) {
+            String countString = String.valueOf(count);
+            for (char c : countString.toCharArray()) {
+                j++;
+                chars[j] = c;
+            }
+        }
+
+        return j + 1;
+        
+    }
+}
