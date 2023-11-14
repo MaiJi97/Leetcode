f08bf1dd7c4c214dcb68dc58e65aeeea1b1439d9
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-11-13 20:57:07 -0600

    Update TrappingRainWater1.java

--- java/42_TrappingRainWater/TrappingRainWater1.java
+++ java/42_TrappingRainWater/TrappingRainWater1.java
@@ -7,10 +7,10 @@ class Solution {
         leftMax[0] = height[0]; 
         rightMax[height.length - 1] = height[height.length - 1];
         for (int i = 1 ; i < height.length; i++) { 
-            leftMax[i] = Math.max(height[i], leftMax[i-1]); // 记录从index i处看到的左边最大高度（包括index i处的高度）
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]); // 记录从index i处看到的左边最大高度（包括index i处的高度）
         }
         for (int i = height.length-2; i >= 0; i--) { 
-            rightMax[i] = Math.max(height[i], rightMax[i+1]); // 记录从index i处看到的右边最大高度（包括index i处的高度）
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]); // 记录从index i处看到的右边最大高度（包括index i处的高度）
         }
         for (int i = 0; i < height.length; i++) {
             ret += (Math.min(leftMax[i], rightMax[i]) - height[i]);
