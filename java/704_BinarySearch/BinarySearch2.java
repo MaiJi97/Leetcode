e39249d913a12153788ddb1747f69a1a317fc413
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-11-15 15:53:21 -0600

    Update BinarySearch2.java

--- java/704_BinarySearch/BinarySearch2.java
+++ java/704_BinarySearch/BinarySearch2.java
@@ -1,15 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
         int start = 0;
-        int end = nums.length;
-        while (start < end) {
+        int end = nums.length - 1;
+        while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (nums[mid] == target) return mid;
+            if (nums[mid] == target) {
+                return mid;
+            }
             else if (nums[mid] < target) {
                 start = mid + 1;
             }
-            else {       
-                end = mid;
+            else {
+                end = mid - 1;
             }
         }
         return -1;
