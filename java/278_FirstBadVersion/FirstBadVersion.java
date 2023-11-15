e426b38a9cd7feca7af3a666561b989f82628a5e
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-11-15 15:41:39 -0600

    Update FirstBadVersion.java

--- java/278_FirstBadVersion/FirstBadVersion.java
+++ java/278_FirstBadVersion/FirstBadVersion.java
@@ -5,14 +5,12 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
-        while (left <= right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
             if (!isBadVersion(mid)) {
                 left = mid + 1;
             }
-            else {
-                right = mid - 1;
-            }
+            else right = mid;
         }
         return left;
     }
