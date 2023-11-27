5ea1ea74a3f299a9cf709c8681ef40a7901de647
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-11-26 22:46:03 -0600

    Update FindFirstAndLastPositionOfElementInSortedArray.java

--- java/34_FindFirstAndLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArray.java
+++ java/34_FindFirstAndLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,51 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int index = -1;
+
         int[] ret = new int[2];
-        ret[0] = searchStartIndex(nums, target);
-        ret[1] = searchEndIndex(nums, target);
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                index = mid;
+                right = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else right = mid - 1;
+
+        }
+        ret[0] = index;
+
+        left = 0;
+        right = nums.length - 1;
+        index = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                index = mid;
+                left = mid + 1;
+            }
+            else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+            else left = mid + 1;
+        }
+        ret[1] = index;
+
         return ret;
-    
-    }
-    
-    public int searchStartIndex(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        int index = -1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
-            }
-            
-            if (nums[mid] == target) {
-                index = mid;
-            }
-        }
-        return index;
-    }
-    
-    public int searchEndIndex(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        int index = -1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > target) {
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
-            }
-            
-            if (nums[mid] == target) {
-                index = mid;
-            }
-        }
-        return index;
+
     }
 }
