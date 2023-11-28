7ee91dff71edfa74969060f85ac546c9a5479a56
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-11-27 18:57:21 -0600

    Update LongestPalindromicSubsequence.java

--- java/516_LongestPalindromeSubsequence/LongestPalindromicSubsequence.java
+++ java/516_LongestPalindromeSubsequence/LongestPalindromicSubsequence.java
@@ -1,25 +1,23 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        if (s.length() <= 1) {
-            return s.length();
-        }
         int n = s.length();
         int[][] dp = new int[n][n];
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
+        int maxlen = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
                 if (s.charAt(i) == s.charAt(j)) {
-                    if (j - i <= 2) {
-                        dp[i][j] = j - i + 1;
+                    if (i - j <= 2) {
+                        dp[i][j] = i - j + 1;
                     }
                     else {
-                        dp[i][j] = dp[i + 1][j - 1] + 2;
-                    }
+                        dp[i][j] = dp[i - 1][j + 1] + 2;
+                    }   
                 }
                 else {
-                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j + 1]);
                 }
             }
         }
-        return dp[0][n-1];
+        return dp[n - 1][0];
     }
 }
