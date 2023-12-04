99a6671f8b14fd6c74f6184d97fa485286140dc7
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-12-04 15:45:44 -0600

    Update SubarraySumEqualsK1.java

--- java/560_SubarraySumEqualsK/SubarraySumEqualsK1.java
+++ java/560_SubarraySumEqualsK/SubarraySumEqualsK1.java
@@ -4,12 +4,12 @@ class Solution {
         HashMap<Integer, Integer> hm = new HashMap<>(); // <prefixSum, frequencies>
         int[] prefixSum = new int[nums.length + 1];
         for (int i = 0; i < nums.length; i++) {
-            prefixSum[i + 1] = prefixSum[i] + nums[i];
+          prefixSum[i + 1] = prefixSum[i] + nums[i];
         }
         for (int i = 0; i < prefixSum.length; i++) {
             int target = prefixSum[i] - k;
             if (hm.containsKey(target)) {
-              ret += hm.get(target);
+                ret += hm.get(target);
             }
             hm.put(prefixSum[i], hm.getOrDefault(prefixSum[i], 0) + 1);
         }
