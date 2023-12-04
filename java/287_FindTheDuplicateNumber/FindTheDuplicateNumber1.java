c7919ab157becee8750cc3ddfad5795688c8269a
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-12-03 18:05:29 -0600

    Update FindTheDuplicateNumber1.java

--- java/287_FindTheDuplicateNumber/FindTheDuplicateNumber1.java
+++ java/287_FindTheDuplicateNumber/FindTheDuplicateNumber1.java
@@ -2,14 +2,14 @@ class Solution {
     public int findDuplicate(int[] nums) { // 1,2,2,3,4 - 5 numbers
         int left = 1;
         int right = nums.length - 1;
-        while (left <= right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
             int count = 0;
             for (int i = 0; i < nums.length; i++) {
                 if (nums[i] <= mid) count++;
             }
             if (count <= mid) left = mid + 1;
-            else right = mid - 1;
+            else right = mid;
         }
         return left;
     }
