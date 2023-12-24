02064ee10b218d95619ec44634984bfda960dbfc
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-12-24 16:55:28 -0600

    Create LeastNumberOfUniqueIntegersAfterKRemovals2.java

---
+++ java/1481_LeastNumberOfUniqueIntegersAfterKRemovals/LeastNumberOfUniqueIntegersAfterKRemovals2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        HashMap<Integer, Integer> frequencies = new HashMap<>(); // <element, frequencies>
+        for (int i = 0; i < arr.length; i++) {
+            frequencies.put(arr[i], frequencies.getOrDefault(arr[i], 0) + 1);
+        }
+        int[] bucketSort = new int[arr.length + 1];
+        for (int key : frequencies.keySet()) {
+            int frequency = frequencies.get(key);
+            bucketSort[frequency]++;
+        }
+        int index = 0;
+        int totalUnique = frequencies.size();
+        while (index < bucketSort.length) {
+            int numOfElement = bucketSort[index];
+            for (int i = 0; i < numOfElement; i++) {
+                if (k - index >= 0) {
+                    k -= index;
+                    totalUnique--;
+                }
+                else break;
+            }
+            index++;
+        }
+        return totalUnique;
+    }
+}
