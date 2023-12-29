610e2901857d578b4b369cd8f0df73f677a4932c
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-12-29 16:40:41 -0600

    Update AsteroidCollision.java

--- java/735_AsteroidCollision/AsteroidCollision.java
+++ java/735_AsteroidCollision/AsteroidCollision.java
@@ -1,20 +1,26 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack = new Stack<>();
-        for (int asteroid : asteroids) {
-            if (stack.isEmpty() || stack.peek()*asteroid > 0 || (stack.peek() < 0 && asteroid > 0)) stack.push(asteroid);
+        for (int i = 0; i < asteroids.length; i++) {
+            int current = asteroids[i];
+            if (stack.isEmpty() || stack.peek() * current > 0 || (stack.peek() < 0 && current > 0)) stack.push(current);
             else {
-                boolean flag = false;
-                while (!stack.isEmpty() && stack.peek() > 0 && asteroid < 0) {
-                    int other = stack.pop();
-                    if (Math.abs(other) > Math.abs(asteroid)) asteroid = other;
-                    else if (Math.abs(other) < Math.abs(asteroid)) continue;
-                    else {
-                        flag = true;
+                boolean flag = true;
+                while (!stack.isEmpty() && stack.peek() > 0 && current < 0) {
+                    int previous = stack.pop();
+                    int update = 0;
+                    if (Math.abs(current) > Math.abs(previous)) update = current;
+                    else if (Math.abs(current) < Math.abs(previous)) update = previous;
+
+                    if (update == 0) {
+                        flag = false;
                         break;
                     }
+                    else {
+                        current = update;
+                    }
                 }
-                if (flag == false) stack.push(asteroid);
+                if (flag == true) stack.push(current);
             }
         }
         int[] ret = new int[stack.size()];
