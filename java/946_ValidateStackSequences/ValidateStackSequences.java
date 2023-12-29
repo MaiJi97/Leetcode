7017e2f7c454d0869ca333ea05b38db519fd112a
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2023-12-28 23:32:07 -0600

    Create ValidateStackSequences.java

---
+++ java/946_ValidateStackSequences/ValidateStackSequences.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int j = 0;
+        for (int i = 0; i < pushed.length; i++) {
+            stack.push(pushed[i]);
+            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
