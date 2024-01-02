d877c603c735b0796db6b836985cff4d4f9682e1
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-01-02 13:40:10 -0600

    Update ReorganizeString.java

--- java/767_ReorganizeString/ReorganizeString.java
+++ java/767_ReorganizeString/ReorganizeString.java
@@ -1,29 +1,26 @@
 class Solution {
     public String reorganizeString(String s) {
         if (s.length() == 0 || s.length() == 1) return s;
-        HashMap<Character, Integer> frequencies = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            frequencies.put(c, frequencies.getOrDefault(c, 0) + 1);
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
         }
         PriorityQueue<Pair<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
-        for (Character key : frequencies.keySet()) {
-            pq.offer(new Pair(key, frequencies.get(key)));
+        for (char c : hm.keySet()) {
+            pq.offer(new Pair<>(c, hm.get(c)));
         }
         StringBuilder sb = new StringBuilder();
-
         Pair<Character, Integer> pair = pq.poll();
         while (!pq.isEmpty()) {
-            sb.append(pair.getKey());
+            int frequency = pair.getValue();
             Pair<Character, Integer> next = pq.poll();
-            int frequencyLeft = (int) pair.getValue() - 1;
-            if (frequencyLeft > 0) {
-                pq.offer(new Pair(pair.getKey(), frequencyLeft));
+            sb.append(pair.getKey());
+            if (frequency - 1 > 0) {
+                pq.offer(new Pair<Character, Integer>(pair.getKey(), frequency - 1));
             }
             pair = next;
         }
         sb.append(pair.getKey());
-        
-        return (int)pair.getValue() > 1 ? "" : sb.toString();
-
+        return pair.getValue() > 1 ? "" : sb.toString();
     }
 }
