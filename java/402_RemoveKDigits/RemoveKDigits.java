ecdaede845b29b5fa35b1611b3679ce34a34c4a9
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-01-05 17:34:35 -0600

    Update RemoveKDigits.java

--- java/402_RemoveKDigits/RemoveKDigits.java
+++ java/402_RemoveKDigits/RemoveKDigits.java
@@ -4,8 +4,8 @@ class Solution {
 
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < num.length(); i++) {
-            // stack is not empty + peek element greater than current element + number of removal is less than k
-            while (!stack.isEmpty() && num.charAt(i) < stack.peek() && k > 0) {
+            // stack is not empty + peek element is smaller than current element + number of replace is less than k
+            while (!stack.isEmpty() && stack.peek() > num.charAt(i) && k > 0) {
                 stack.pop();
                 k--;
             }
@@ -32,7 +32,7 @@ class Solution {
             if (temp.charAt(i) == '0' && retBuilder.length() == 0) continue;
             retBuilder.append(temp.charAt(i));
         }
+        
 
         return retBuilder.length() == 0 ? "0" : retBuilder.toString();
     }
-}
