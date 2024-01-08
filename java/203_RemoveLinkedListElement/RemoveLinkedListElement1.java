59b30bb5c0b11db9d207622c6326a3132bdd6b60
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-01-08 17:29:07 -0600

    Update RemoveLinkedListElement1.java

--- java/203_RemoveLinkedListElement/RemoveLinkedListElement1.java
+++ java/203_RemoveLinkedListElement/RemoveLinkedListElement1.java
@@ -10,17 +10,13 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-
         ListNode dummy = new ListNode(-1, head);
-        
         ListNode current = dummy;
         while (current != null) {
             if (current.next != null && current.next.val == val) {
                 current.next = current.next.next;
             }
-            else {
-                current = current.next;
-            }
+            else current = current.next;
         }
         return dummy.next;
     }
