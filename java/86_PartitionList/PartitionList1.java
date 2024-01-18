4bca897de3f07ffee2852587240438cca6ff4097
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-01-18 17:22:23 -0600

    Update PartitionList1.java

--- java/86_PartitionList/PartitionList1.java
+++ java/86_PartitionList/PartitionList1.java
@@ -14,17 +14,16 @@ class Solution {
         ListNode dummy2 = new ListNode(-1);
         ListNode dummy1Pointer = dummy1;
         ListNode dummy2Pointer = dummy2;
-        ListNode current = head;
-        while (current != null) {
-            if (current.val < x) {
-                dummy1Pointer.next = new ListNode(current.val);
+        while (head != null) {
+            if (head.val < x) {
+                dummy1Pointer.next = new ListNode(head.val);
                 dummy1Pointer = dummy1Pointer.next;
             }
             else {
-                dummy2Pointer.next = new ListNode(current.val);
+                dummy2Pointer.next = new ListNode(head.val);
                 dummy2Pointer = dummy2Pointer.next;
             }
-            current = current.next;
+            head = head.next;
         }
         dummy2Pointer.next = null;
         dummy1Pointer.next = dummy2.next;
