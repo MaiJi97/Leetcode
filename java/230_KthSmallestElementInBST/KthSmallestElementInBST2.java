cb51922c47dd55b329016f50c2c04563d1174406
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-01-18 23:06:59 -0600

    Update KthSmallestElementInBST2.java

--- java/230_KthSmallestElementInBST/KthSmallestElementInBST2.java
+++ java/230_KthSmallestElementInBST/KthSmallestElementInBST2.java
@@ -1,18 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
         Stack<TreeNode> stack = new Stack<>();
-        while (!stack.isEmpty() || root != null) {
-            while(root != null) {
-                stack.push(root);
-                root = root.left;
+        TreeNode current = root;
+        while (!stack.isEmpty() || current != null) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
             }
-            root = stack.pop();
+            current = stack.pop();
             k--;
-            if(k == 0) {
-                return root.val;
+            if (k == 0) {
+                return current.val;
             }
-            root = root.right;
+            current = current.right;
         }
-        return 0;
+        return -1;
     }
 }
