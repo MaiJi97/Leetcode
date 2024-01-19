559bdca4c1eb5262d1a644f6ec606a5bb49f5e82
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-01-18 23:02:27 -0600

    Create BinaryTreePostorderTraversalStack.java

---
+++ java/144_BinaryTreePreorderTraversal/BinaryTreePostorderTraversalStack.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ret = new ArrayList<>();
+        if (root == null) return ret;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+            ret.add(0, current.val);
+            if (current.left != null) stack.push(current.left);
+            if (current.right != null) stack.push(current.right); 
+        }
+        return ret;
+    }
+}
