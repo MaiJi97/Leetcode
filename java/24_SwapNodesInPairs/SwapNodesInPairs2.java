978eb741ea054f36ab650fb29dada30efc6b06ca
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-01-22 15:12:42 -0600

    Create SwapNodesInPairs2.java

---
+++ java/24_SwapNodesInPairs/SwapNodesInPairs2.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode restPairs = swapPairs(head.next.next);
+        ListNode second = head.next;
+        second.next = head;
+        head.next = restPairs;
+        return second;
+    }
+}
