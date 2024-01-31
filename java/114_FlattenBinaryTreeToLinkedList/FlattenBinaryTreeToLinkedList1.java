958c48a51979027288607997b479a8b91a34a870
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-01-30 20:34:16 -0600

    Create FlattenBinaryTreeToLinkedList1.java

---
+++ java/114_FlattenBinaryTreeToLinkedList/FlattenBinaryTreeToLinkedList1.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        Stack<TreeNode> stack = new Stack<>();
+
+        stack.push(root);
+
+        TreeNode prev = null;
+
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+            if (prev != null) {
+                prev.right = current;
+                prev.left = null;
+            }
+
+            if (current.right != null) stack.push(current.right);
+            if (current.left != null) stack.push(current.left);
+
+            prev = current;
+        }
+    }
+}
