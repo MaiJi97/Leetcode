fc76050940869271023001a9c99721094cae7524
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-02-20 12:42:26 -0600

    Update InsertDeleteGetRandom2.java

--- java/380_InsertDeleteGetRandom/InsertDeleteGetRandom2.java
+++ java/380_InsertDeleteGetRandom/InsertDeleteGetRandom2.java
@@ -1,41 +1,39 @@
 class RandomizedSet {
 
     ArrayList<Integer> list; 
-    HashMap<Integer, Integer> hm; // <element, index>
+    HashMap<Integer, Integer> hm;
+    int index;
     java.util.Random rand = new java.util.Random();
 
     public RandomizedSet() {
         this.list = new ArrayList<>();
-        this.hm = new HashMap<>(); 
+        this.hm = new HashMap<>();
+        this.index = 0;
     }
     
     public boolean insert(int val) {
        if (hm.containsKey(val)) return false;
        list.add(val);
-       hm.put(val, list.size() - 1);
+       hm.put(val, index);
+       index++;
        return true;
     }
     
     public boolean remove(int val) {
         if (!hm.containsKey(val)) return false;
-        swap(hm.get(val), list.size() - 1);
+        int lastElement = list.get(list.size() - 1);
+        int valIndex = hm.get(val);
+        list.set(valIndex, lastElement);
         list.remove(list.size() - 1);
+        hm.put(lastElement, valIndex); // put first, in case there is only one element 
         hm.remove(val);
+        index--;
         return true;
     }
     
     public int getRandom() {
         int index = rand.nextInt(list.size());
         return list.get(index);
-    }
-
-    public void swap(int indexToRemove, int indexLast) {
-        int elementToRemove = list.get(indexToRemove);
-        int elementLast = list.get(indexLast);
-        list.set(indexToRemove, elementLast);
-        list.set(indexLast, elementToRemove);
-        hm.put(elementToRemove, indexLast);
-        hm.put(elementLast, indexToRemove);
     }
 }
 
