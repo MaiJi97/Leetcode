6e031c2fe5058410582fb15f29e3a7edec89cc56
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-03-01 17:31:26 -0600

    Update Triangle1.java

--- java/120_Triangle/Triangle1.java
+++ java/120_Triangle/Triangle1.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        return dfs(triangle, 0, 0, 0);
+        return dfs(triangle, 0, 0);
     }
 
-    public int dfs(List<List<Integer>> triangle, int i, int j, int currentSum) {
-        if (i == triangle.size()) return currentSum;
+    public int dfs(List<List<Integer>> triangle, int i, int j) {
+        if (i == triangle.size() - 1) return triangle.get(i).get(j);
         else {
             return Math.min(dfs(triangle, i + 1, j, currentSum + triangle.get(i).get(j)), dfs(triangle, i + 1, j + 1, currentSum + triangle.get(i).get(j)));
         }
