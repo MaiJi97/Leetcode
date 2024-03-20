de234ac782015959717267a8455b6719f8a63237
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-03-19 21:32:52 -0500

    Update DeleteAndEarn1.java

--- java/740_DeleteAndEarn/DeleteAndEarn1.java
+++ java/740_DeleteAndEarn/DeleteAndEarn1.java
@@ -8,17 +8,19 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             bucketSort[nums[i]]++;
         }
+        
         int[] memo = new int[max + 1];
         Arrays.fill(memo, -1);
         return maxEarn(bucketSort, max, memo);
     }
 
-    public int maxEarn(int[] bucketSort, int index, int[] memo) {
-        if (bucketSort.length == 0) return 0;
-        if (index == 0 || index == 1) return bucketSort[index] * index;
-        else if (memo[index] != -1) return memo[index];
-        int earn = Math.max((maxEarn(bucketSort, index - 2, memo) + bucketSort[index] * index), maxEarn(bucketSort, index - 1, memo));
-        memo[index] = earn;
-        return earn;
+    public int maxEarn(int[] bucketSort, int max, int[] memo) {
+        if (max == 0) return memo[0] = 0;
+        else if (max == 1) return memo[1] = bucketSort[1] * 1;
+        else if (memo[max] != -1) return memo[max];
+        else {
+            memo[max] = Math.max(maxEarn(bucketSort, max - 1, memo), maxEarn(bucketSort, max - 2, memo) + bucketSort[max] * max);
+            return memo[max];
+        }
     }
 }
