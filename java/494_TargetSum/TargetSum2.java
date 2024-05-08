8cb899d2c33c617f739a4270d42ffcfe4036b418
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-05-08 14:43:44 -0500

    Update TargetSum2.java

--- java/494_TargetSum/TargetSum2.java
+++ java/494_TargetSum/TargetSum2.java
@@ -3,25 +3,17 @@ class Solution {
         HashMap<String, Integer> memo = new HashMap<>();
         return dfs(nums, target, 0, 0, memo);
     }
-    
-    public int dfs(int[] nums, int target, int index, int sum, HashMap<String, Integer> memo) {
-        String key = serialize(index, sum);
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
+
+    public int dfs(int[] nums, int target, int index, int currentSum, HashMap<String, Integer> memo) {
+        String key = index + "," + currentSum;
+        if (memo.containsKey(key)) return memo.get(key);
         if (index == nums.length) {
-            if (sum == target) {
-                return 1;
-            }
-            return 0;
+            if (currentSum == target) return 1;
+            else return 0;
         }
-        int plus = dfs(nums, target, index + 1, sum + nums[index], memo);
-        int minus = dfs(nums, target, index + 1, sum - nums[index], memo);
-        memo.put(key, plus+minus);
-        return plus+minus;
-    }
-    
-    public String serialize(int index, int sum) {
-        return index + "," + sum;
+        int plus = dfs(nums, target, index + 1, currentSum + nums[index], memo);
+        int minus = dfs(nums, target, index + 1, currentSum - nums[index], memo);
+        memo.put(key, plus + minus);
+        return plus + minus;
     }
 }
