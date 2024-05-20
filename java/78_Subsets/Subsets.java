e29bd450fb1f974ef59c243a1b48c696657da13a
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-05-20 13:43:37 -0500

    Update Subsets.java

--- java/78_Subsets/Subsets.java
+++ java/78_Subsets/Subsets.java
@@ -1,16 +1,18 @@
 class Solution {
+
+    List<List<Integer>> ret = new ArrayList<>();
+
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ret = new ArrayList<>();
-        dfs(nums, ret, new ArrayList<>(), 0);
+        dfs(nums, new ArrayList<>(), 0);
         return ret;
     }
 
-    public void dfs(int[] nums, List<List<Integer>> ret, List<Integer> current, int start) {
+    public void dfs(int[] nums, List<Integer> current, int start) {
         ret.add(new ArrayList<>(current)); // 不论current是什么直接加入结果
 
         for (int i = start; i < nums.length; i++) {
             current.add(nums[i]);
-            dfs(nums, ret, current, i + 1);
+            dfs(nums, current, i+1);
             current.remove(current.size() - 1);
         }
     }
