467e242e974a532c21ed091a6c13d16c3ecabe19
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-05-22 16:17:17 -0500

    Create NestedListWeightSum.java

---
+++ java/339_NestedListWeightSum/NestedListWeightSum.java
@@ -0,0 +1,45 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        return dfs(nestedList, 1);
+    }
+
+    public int dfs(List<NestedInteger> nestedList, int currentDepth) {
+        if (nestedList.size() == 0) {
+            return 0;
+        }
+        if (nestedList.get(0).isInteger()) {
+            return nestedList.get(0).getInteger() * currentDepth + dfs(nestedList.subList(1, nestedList.size()), currentDepth);
+        }
+        else {
+            return dfs(nestedList.get(0).getList(), currentDepth + 1) + dfs(nestedList.subList(1, nestedList.size()), currentDepth);
+        }
+    }
+}
