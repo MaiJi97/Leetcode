b32cab42da64c6d58fbcbd6cdaabedd571c5d1ee
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-05-24 15:23:28 -0500

    Create Subsets2.java

---
+++ java/78_Subsets/Subsets2.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    List<List<Integer>> ret = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(nums, new ArrayList<>(), 0);
+        return ret;
+    }
+
+    public void dfs(int[] nums, List<Integer> current, int start) {
+        if (start == nums.length) {
+            ret.add(new ArrayList<>(current));
+            return;
+        }
+        current.add(nums[start]);
+        dfs(nums, current, start + 1);
+        current.remove(current.size() - 1);
+        
+        dfs(nums, current, start + 1);
+    }
+}
