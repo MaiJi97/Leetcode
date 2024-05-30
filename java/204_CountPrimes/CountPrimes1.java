eb9cb9c417382438b512f9713e2ec1600be4274f
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-05-30 16:19:32 -0500

    Create CountPrimes1.java

---
+++ java/204_CountPrimes/CountPrimes1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+        int ret = 1;
+        for (int i = 3; i < n; i++) {
+            boolean flag = false;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) ret++;
+        }
+        return ret;
+    }
+}
