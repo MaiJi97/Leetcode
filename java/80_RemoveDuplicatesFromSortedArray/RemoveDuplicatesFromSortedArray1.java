f6b223207bfcf2788029989cae4a299b552f84d5
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-05-30 16:53:14 -0500

    Update RemoveDuplicatesFromSortedArray1.java

--- java/80_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray1.java
+++ java/80_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray1.java
@@ -1,22 +1,19 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int j = 0;
         int count = 1;
+        int j = 0;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[j]) {
-                j++;
-                nums[j] = nums[i];
-                count = 1;
-            }
-            else {
+            if (nums[i] == nums[j]) {
                 if (count < 2) {
                     j++;
                     nums[j] = nums[i];
                     count++;
                 }
-                else {
-                    count++;
-                }
+            }
+            else {
+                j++;
+                nums[j] = nums[i];
+                count = 1;
             }
         }
         return j + 1;
