ea014be1d47d77c1b0c6f281af91aacc56fb8432
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-05-30 17:00:15 -0500

    Update RemoveDuplicatesFromSortedArray2.java

--- java/80_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray2.java
+++ java/80_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray2.java
@@ -1,19 +1,17 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 0 || nums.length == 1) {
-            return nums.length;
-        }
-        int i = 0;
         int j = 0;
+        int i = 0;
         while (i < nums.length) {
             int current = nums[i];
             int count = 0;
             while (i < nums.length && nums[i] == current) {
                 if (count < 2) {
-                    nums[j++] = current;
+                    nums[j] = current;
+                    j++;
+                    count++;
                 }
                 i++;
-                count++;
             }
         }
         return j;
