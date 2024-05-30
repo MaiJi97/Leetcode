d509fb59cbe4615666455119825a1c758236c049
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-05-30 16:25:29 -0500

    Update CountPrimes1.java

--- java/204_CountPrimes/CountPrimes1.java
+++ java/204_CountPrimes/CountPrimes1.java
@@ -3,7 +3,7 @@ class Solution {
         if (n <= 2) return 0;
         int ret = 1;
         for (int i = 3; i < n; i++) {
-            boolean flag = false;
+            boolean flag = false; // inner operation can also be wrapped into a helper method isPrime()
             for (int j = 2; j * j <= i; j++) {
                 if (i % j == 0) {
                     flag = true;
