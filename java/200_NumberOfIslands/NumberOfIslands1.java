14954aa2a95ae09bf33f0335737b1033bc7deb07
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-06-12 15:25:32 -0500

    Update NumberOfIslands1.java

--- java/200_NumberOfIslands/NumberOfIslands1.java
+++ java/200_NumberOfIslands/NumberOfIslands1.java
@@ -1,15 +1,14 @@
 class Solution {
     
-    class Coordinate {
+    int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        
+        class Coordinate {
         int x, y;
         public Coordinate(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
-    
-    int[] deltaX = {0, 1, 0, -1};
-    int[] deltaY = {1, 0, -1, 0};
         
     public int numIslands(char[][] grid) {
         int count = 0;
@@ -18,12 +17,12 @@ class Solution {
         }
         int m = grid.length;
         int n = grid[0].length;    
-        boolean[][] visited = new boolean[m][n]; 
+        boolean[][] visited = new boolean[m][n];
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (visited[i][j] == false && grid[i][j] == '1') {
-                    bfs(i, j, grid, visited); // we are using the same visited matrix each time we call bfs()
+                    bfs(i, j, grid, visited);
                     count++;
                 }
             }
@@ -33,18 +32,18 @@ class Solution {
     
     private void bfs(int i, int j, char[][] grid, boolean[][] visited) {
         Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{i, j});
         visited[0][0] = true;
+        q.offer(new int[]{i, j});
         while (!q.isEmpty()) {
             int[] current = q.poll();
-            for (int direction = 0; direction < 4; direction++) {
-                int newX = current[0] + deltaX[direction];
-                int newY = current[1] + deltaY[direction];
+            for (int[] direction : directions) {
+                int newX = current[0] + direction[0];
+                int newY = current[1] + direction[1];
                 if (newX < 0 || newX >= grid.length || newY < 0 || newY >= grid[0].length || grid[newX][newY] == '0' || visited[newX][newY] == true) {
                     continue;
                 }
-                q.offer(new int[]{newX, newY});
                 visited[newX][newY] = true;
+                q.offer(new int[]{newX, newY});
             }
         }
     }
