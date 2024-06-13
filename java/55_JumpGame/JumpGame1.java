acaba4b6fbb86d1fb5f255099bca99d5617a84d6
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-06-13 16:32:13 -0500

    Update JumpGame1.java

--- java/55_JumpGame/JumpGame1.java
+++ java/55_JumpGame/JumpGame1.java
@@ -1,15 +1,28 @@
 class Solution {
+
     public boolean canJump(int[] nums) {
        boolean[] dp = new boolean[nums.length];
        dp[0] = true;
-       for (int i = 0; i < nums.length; i++) {
-            if (dp[i] == true) {
-                for (int j = 0; j <= nums[i]; j++) {
-                    if (i + j >= nums.length) break;
-                    dp[i + j] = true;
-                }
-            }
-       }
+
+       for (int i = 1; i < nums.length; i++) {
+           for (int j = 0; j <= i; j++) {
+               if (dp[j] == true && i - j <= nums[j]) dp[i] = true;
+           }
+       } 
        return dp[nums.length - 1];
     }
+    
+    // public boolean canJump(int[] nums) {
+    //    boolean[] dp = new boolean[nums.length];
+    //    dp[0] = true;
+    //    for (int i = 0; i < nums.length; i++) {
+    //         if (dp[i] == true) {
+    //             for (int j = 0; j <= nums[i]; j++) {
+    //                 if (i + j >= nums.length) break;
+    //                 dp[i + j] = true;
+    //             }
+    //         }
+    //    }
+    //    return dp[nums.length - 1];
+    // }
 }
