677893edc4271cbef25e33f5e1f1376249f15896
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-06-19 14:00:01 -0500

    Update WordSearch1.java

--- java/79_WordSearch/WordSearch1.java
+++ java/79_WordSearch/WordSearch1.java
@@ -1,30 +1,32 @@
 class Solution {
+    int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public boolean exist(char[][] board, String word) {
-
         int m = board.length;
         int n = board[0].length;
         boolean[][] visited = new boolean[m][n];
-
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (dfs(board, visited, word, i, j, 0)) return true;
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(board, word, i, j, 0, visited)) return true;
+                }
             }
         }
         return false;
     }
 
-    public boolean dfs(char[][] board, boolean[][] visited, String word, int i, int j, int current) {
-        if (current == word.length()) return true;
-
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || word.charAt(current) != board[i][j]) {
+    public boolean dfs(char[][] board, String word, int i, int j, int index, boolean[][] visited) {
+        if (index == word.length()) return true;
+        else if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] == true || word.charAt(index) != board[i][j]) {
             return false;
         }
-
         visited[i][j] = true;
-
-        boolean ret = dfs(board, visited, word, i+1, j, current + 1) || dfs(board, visited, word, i-1, j, current + 1) || dfs(board, visited, word, i, j+1, current + 1) || dfs(board, visited, word, i, j-1, current + 1);
-        
+        for (int[] direction : directions) {
+            int newX = i + direction[0];
+            int newY = j + direction[1];
+            if (dfs(board, word, newX, newY, index + 1, visited)) return true;
+        }
         visited[i][j] = false;
-        return ret;
+        return false;
     }
+    
 }
