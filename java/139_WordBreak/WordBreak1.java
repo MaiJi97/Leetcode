bc5bd9b62def798e77dac6251949304c6b545f4a
MaiJi97 <106039830+MaiJi97@users.noreply.github.com>
2024-07-14 14:48:41 -0500

    Update WordBreak1.java

--- java/139_WordBreak/WordBreak1.java
+++ java/139_WordBreak/WordBreak1.java
@@ -2,18 +2,18 @@ class Solution {
 
     public boolean wordBreak(String s, List<String> wordDict) {
         HashSet<String> hs = new HashSet<>();
+        HashMap<String, Boolean> memo = new HashMap<>();
         for (String word : wordDict) hs.add(word);
-        return dfs(s, hs);
+        return dfs(s, hs, memo);
     }
 
-    public boolean dfs(String s, HashSet<String> hs) {
-        if (hs.contains(s)) {
-            memo.put(s, true);
+    public boolean dfs(String s, HashSet<String> hs, HashMap<String, Boolean> memo) {
+        if (s.length() == 0) {
             return true;
         }
         else if (memo.containsKey(s)) return memo.get(s);
-        for (int i = 1; i < s.length(); i++) {
-            if (hs.contains(s.substring(0, i)) && dfs(s.substring(i), hs)) {
+        for (int i = 1; i <= s.length(); i++) {
+            if (hs.contains(s.substring(0, i)) && dfs(s.substring(i), hs, memo)) {
                 memo.put(s, true);
                 return true;
             }
